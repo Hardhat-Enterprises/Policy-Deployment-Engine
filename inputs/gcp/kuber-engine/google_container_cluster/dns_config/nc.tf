cea49ee5ad7d1e90fe6a08530a1be6cae37dc054
Samira <fallah.samira.1990@gmail.com>
2025-04-25 10:30:54 +1000

    feat: add noncompliance check logic for cluster_dns configuration in nc.tf

--- inputs/gcp/kuber-engine/google_container_cluster/dns_config/nc.tf
+++ inputs/gcp/kuber-engine/google_container_cluster/dns_config/nc.tf
@@ -2,6 +2,19 @@
 # Keep "nc" as the name to indicate that this resource and its attributes are non-compliant
 
 resource "dns_config" "nc" {
-    
   
+  name     = "gke-policy-cluster"
+  location = "AU"
+
+  dns_config {
+    additive_vpc_scope_dns_domain = "gke.bad-domain.com"  
+    cluster_dns                   = "PLATFORM_DEFAULT"     
+    cluster_dns_scope             = "CLUSTER_SCOPE"       
+    cluster_dns_domain            = "example.local"       
+  }
+
+  gateway_api_config {
+    channel = "CHANNEL_DISABLED"  
+  }
 }
+  
