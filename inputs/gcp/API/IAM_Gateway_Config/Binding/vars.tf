94f260a6ec04e94bd2ba7d7ccf7b92ddbcae81b9
Manika2003 <manikabhatia2003@gmail.com>
2025-05-30 22:43:23 +1000

    Remove IAM_Gateway_Config files mistakenly included in managed_service_configs branch

--- inputs/gcp/API/IAM_Gateway_Config/Binding/vars.tf
+++
@@ -1,7 +0,0 @@
-# Variables for IAM Gateway Config Binding Resource
-
-variable "api_config_path" {
-  description = "Full path to the API config"
-  default     = "projects/terraform-456801/locations/global/apis/sample-api/configs/sample-config"
-}
-
--- inputs/gcp/API/IAM_Gateway_Config/Member/vars.tf
+++
@@ -1,7 +0,0 @@
-# Variables for IAM Gateway Config Resource
-
-variable "api_path" {
-  description = "The API this config belongs to"
-  default     = "projects/api_cfg/locations/AU/apis/sample-api-compliant"
-}
-
