7dcce37a97fdb5b2eecbca71240b96bd48752d70
MenuraDewalegama <menuradewalegama@gmail.com>
2025-08-28 12:33:55 +1000

    removing NSR policies

--- inputs/gcp/bigquery_data_policy/google_bigquery_datapolicy_data_policy/data_policy_id/c.tf
+++
@@ -1,7 +0,0 @@
-resource "google_bigquery_datapolicy_data_policy" "c" {
-  location         = "us-central1"
-  data_policy_id   = "data_policy"
-  policy_tag       = "Big Query"
-  data_policy_type = "COLUMN_LEVEL_SECURITY_POLICY"
-  project          = "PDE"
-}
