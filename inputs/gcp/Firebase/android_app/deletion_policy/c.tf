0d13b8b336d936f2390e9747f8d40dcc0e544a6d
waffa <waffa_jabbar@yahoo.com>
2025-09-02 11:33:43 +1000

    Completed GCP Firebase Service V1

--- inputs/gcp/Firebase/android_app/deletion_policy/c.tf
+++ inputs/gcp/Firebase/android_app/deletion_policy/c.tf
@@ -1,5 +1,3 @@
-# Describe your resource type here
-# Keep "c" as the name to indicate that this resource and its attributes are compliant
 
 resource "google_firebase_android_app" "c" {
     display_name = "testfirebaseandroidapp-c"
--- inputs/gcp/Firebase/android_app/deletion_policy/nc.tf
+++ inputs/gcp/Firebase/android_app/deletion_policy/nc.tf
@@ -1,5 +1,3 @@
-# Describe your resource type here
-# Keep "nc" as the name to indicate that this resource and its attributes are non-compliant
 
 resource "google_firebase_android_app" "nc" {
     display_name = "testfirebaseandroidapp-nc"
