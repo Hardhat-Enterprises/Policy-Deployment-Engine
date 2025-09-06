e9b1d715f5044ff43479693caf8d6a91888d1efc
Sebastian <sebastianedge50@gmail.com>
2025-09-06 16:28:12 +1000

    custiom_actions now uses whitelist for certain actions

--- inputs/gcp/deploy/custom_target_type/custom_actions/c.tf
+++ inputs/gcp/deploy/custom_target_type/custom_actions/c.tf
@@ -1,9 +1,6 @@
-# Describe your resource type here
-# Keep "c" as the name to indicate that this resource and its attributes are compliant
-
 resource "google_clouddeploy_custom_target_type" "c" {
   location = "us-central1"
-  name     = "test-custom-target-c"
+  name     = "c"
   project  = "my-project-name"
   
   custom_actions {
--- inputs/gcp/deploy/custom_target_type/custom_actions/nc.tf
+++
@@ -1,8 +0,0 @@
-# Describe your resource type here
-# Keep "nc" as the name to indicate that this resource and its attributes are non-compliant
-
-resource "google_clouddeploy_custom_target_type" "nc" {
-  location = "us-central1"
-  name     = "test-custom-target-nc"
-  project  = "my-project-name"
-}
