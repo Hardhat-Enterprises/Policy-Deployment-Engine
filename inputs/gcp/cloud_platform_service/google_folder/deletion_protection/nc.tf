d5af8cafef5108daf6a06594f9abfcfaa85c748b
vinvincodes <vjoscey@gmail.com>
2025-09-14 10:12:30 +1000

    Save local changes before merging

--- inputs/gcp/cloud_platform_service/google_folder/deletion_protection/nc.tf
+++ inputs/gcp/cloud_platform_service/google_folder/deletion_protection/nc.tf
@@ -2,12 +2,10 @@ resource "google_folder" "NC1" {
   display_name        = "NonCompliant Folder"
   parent              = "organizations/1234567"
   deletion_protection = false   # ❌ Should trigger policy violation
-  # name = "Non compliant folder"
 }
 
 resource "google_folder" "NC2" {
   display_name        = "Department B Folder"
   parent              = "DepartmentB/1234567"
   deletion_protection = false   # ❌ Should trigger policy violation
-  # name = "Department B Folder"
 }
