4604fbdd647333fb3d17ed340934839505c6b384
chunyi wang <whunyi@foxmail.com>
2025-09-15 17:02:40 +1000

    update name attribute values and docs for memcache

---
+++ inputs/gcp/memcache/google_memcache_instance/reserved_ip_range_id/nc.tf
@@ -0,0 +1,24 @@
+resource "google_memcache_instance" "nc1" {
+    project        = "abc-12345678"
+    name           = "nc1" 
+    node_count     = 1
+
+    node_config {
+        cpu_count      = 1
+        memory_size_mb = 1024
+    }
+
+}
+
+resource "google_memcache_instance" "nc2" {
+    project        = "abc-12345678"
+    name           = "nc2" 
+    node_count     = 1
+
+    node_config {
+        cpu_count      = 1
+        memory_size_mb = 1024
+    }
+
+    reserved_ip_range_id = []
+}
