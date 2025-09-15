0eccaaf29cce40b59292135088ac8f31c7b3c4ca
LegendaryMercury <ljfastell@gmail.com>
2025-09-15 17:50:45 +1000

    More Changes
    More Changes Made

--- inputs/gcp/discovery_engine/cmek_config/cmek_config_single_region_keys/nc.tf
+++ inputs/gcp/discovery_engine/cmek_config/cmek_config_single_region_keys/nc.tf
@@ -6,14 +6,14 @@ resource "google_discovery_engine_cmek_config" "nc" {
   cmek_config_id = "nc"
 
   # Default CMEK for multi-regional "eu"
-  kms_key        = "projects/735927692082/locations/eu/keyRings/my-ring/cryptoKeys/my-us-key"
+  kms_key        = ""
 
   project        = "735927692082"
   set_default    = true
 
   # Single-region CMEKs
   single_region_keys {
-    kms_key = "projects/735927692082/locations/europe-west1/keyRings/my-ring/cryptoKeys/my-us1-key"
+    kms_key = ""
   }
 }
 
--- policies/gcp/discovery_engine/data_store/data_store_document_processing_config/policy.rego
+++ policies/gcp/discovery_engine/data_store/data_store_document_processing_config/policy.rego
@@ -13,7 +13,7 @@ conditions := [
         "remedies": ["Ensure that it is set to true"]
         },
       {
-        "condition": "allow_cross_region is mis-configured",
+        "condition": "document_processing_config is mis-configured",
         "attribute_path": ["document_processing_config", 0, "parsing_config_overrides", 0, "ocr_parsing_config", 0, "use_native_text"],
         "values": [false],
         "policy_type": "blacklist"
