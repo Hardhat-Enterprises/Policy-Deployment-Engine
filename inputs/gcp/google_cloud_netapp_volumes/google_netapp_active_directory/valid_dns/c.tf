5e941c1e1901a71f4320de91241dd2806a1da464
AK199417 <arulrajah.kumaraguruparar@gmail.com>
2025-09-17 17:44:23 +1000

    in ctf value was wrong

--- inputs/gcp/google_cloud_netapp_volumes/google_netapp_active_directory/valid_dns/c.tf
+++ inputs/gcp/google_cloud_netapp_volumes/google_netapp_active_directory/valid_dns/c.tf
@@ -3,7 +3,7 @@ resource "google_netapp_active_directory" "c1" {
   name = "c1"
   location = "us-central1"
   domain = "deakin.internal"
-  dns = "10.10.0.10,10.10.0.11"
+  dns = "10.10.0.10"
   net_bios_prefix = "smbserver"
   username = "user"
   password = "pass"
