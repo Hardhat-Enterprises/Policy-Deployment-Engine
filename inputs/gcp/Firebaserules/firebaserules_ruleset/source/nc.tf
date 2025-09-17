7b6067b602432639301b7c8dcdedb34a4ce8b9eb
Guandong <s223930185@deakin.edu.au>
2025-09-17 13:20:32 +0800

    chore: remove all .DS_Store files

--- inputs/gcp/Firebaserules/firebaserules_ruleset/source/nc.tf
+++
@@ -1,6 +0,0 @@
-resource "google_firebaserules_ruleset" "ruleset_nc" {
-  # 完全缺失 source 块 → 语言、文件均空，policy 会报缺失
-  project = data.google_project.project.project_id
-}
-
-data "google_project" "project" {}
