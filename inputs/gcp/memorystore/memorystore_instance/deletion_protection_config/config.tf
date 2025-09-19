581988e05e9bfb1ab522efe962b5b33a3c9b8686
Guandong <s223930185@deakin.edu.au>
2025-09-19 19:17:03 +0800

    fix bug:Terraform failed to compile![2.0]

--- inputs/gcp/memorystore/memorystore_instance/deletion_protection_config/config.tf
+++ inputs/gcp/memorystore/memorystore_instance/deletion_protection_config/config.tf
@@ -2,8 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
+      version = ">= 6.15.0"
     }
   }
+
+   required_version = ">= 1.3.0"
 }
 
 provider "google" {}
