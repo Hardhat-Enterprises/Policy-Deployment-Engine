fb1ac4ac48644a5e82a878200cd659651672f23b
Junyan <s223943884@deakin.edu.au>
2025-09-22 22:22:53 +0800

    fix bugs: Service: firestore | Resource: firestore_backup_schedule | Policy: daily_recurrence Terraform failed to compile!

--- inputs/gcp/firestore/firestore_backup_schedule/daily_recurrence/c.tf
+++ inputs/gcp/firestore/firestore_backup_schedule/daily_recurrence/c.tf
@@ -1,11 +1,11 @@
 # resource "google_firestore_backup_schedule" "c" {
-resource "google_firestore_database" "c" {
-  project  = "abcd1234"
-  # database = google_firestore_database.database.name
-  # database = google_firestore_database.dc.name
-  retention = "8467200s" // 14 weeks (maximum possible retention)
-  daily_recurrence {}
-}
+# resource "google_firestore_database" "c" {
+#   project  = "abcd1234"
+#   # database = google_firestore_database.database.name
+#   # database = google_firestore_database.dc.name
+#   retention = "8467200s" // 14 weeks (maximum possible retention)
+#   daily_recurrence {}
+# }
 
 # resource "google_firestore_database" "database1" {
 #   project     = "abcd_1234"
@@ -17,4 +17,23 @@ resource "google_firestore_database" "c" {
 #   delete_protection_state = "DELETE_PROTECTION_ENABLED"
 #   deletion_policy         = "DELETE"
 # }
+
+resource "google_firestore_database" "database" {
+  project     = "abcd1234"
+  name        = "database-id"
+  location_id = "nam5"
+  type        = "FIRESTORE_NATIVE"
+
+  # delete_protection_state = "DELETE_PROTECTION_ENABLED"
+  # deletion_policy         = "DELETE"
+}
+
+resource "google_firestore_backup_schedule" "c" {
+  project  = "abcd1234"
+  database = google_firestore_database.database.name
+
+  retention = "8467200s" // 14 weeks (maximum possible retention)
+
+  daily_recurrence {}
+}
 
--- inputs/gcp/firestore/firestore_backup_schedule/daily_recurrence/nc.tf
+++ inputs/gcp/firestore/firestore_backup_schedule/daily_recurrence/nc.tf
@@ -1,11 +1,11 @@
 # resource "google_firestore_backup_schedule" "nc" {
-resource "google_firestore_database" "c" {
-  project  = "abcd1234"
-  # database = google_firestore_database.database.name
-  # database = google_firestore_database.dnc.name
-  retention = "8467200s" // 14 weeks (maximum possible retention)
-  # daily_recurrence {}
-}
+# resource "google_firestore_database" "c" {
+#   project  = "abcd1234"
+#   # database = google_firestore_database.database.name
+#   # database = google_firestore_database.dnc.name
+#   retention = "8467200s" // 14 weeks (maximum possible retention)
+#   # daily_recurrence {}
+# }
 
 # resource "google_firestore_database" "database2" {
 #   project     = "abcd_1234"
@@ -18,3 +18,21 @@ resource "google_firestore_database" "c" {
 #   //deletion_policy         = "DELETE"
 # }
 
+resource "google_firestore_database" "database_nc" {
+  project     = "abcd1234"
+  name        = "database-id"
+  location_id = "nam5"
+  type        = "FIRESTORE_NATIVE"
+
+  # delete_protection_state = "DELETE_PROTECTION_ENABLED"
+  # deletion_policy         = "DELETE"
+}
+
+resource "google_firestore_backup_schedule" "nc" {
+  project  = "abcd1234"
+  database = google_firestore_database.database_nc.name
+
+  retention = "8467200s" // 14 weeks (maximum possible retention)
+
+  daily_recurrence {}
+}
