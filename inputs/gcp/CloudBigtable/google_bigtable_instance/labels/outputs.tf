ef9d9ab1667ccf87cf7e71b7c1c3e01f8356cc55
adithya1811 <adithyasubramani.s@gmail.com>
2025-09-24 10:24:08 +1000

    Feedback addressed policies

--- inputs/gcp/CloudBigtable/google_bigtable_instance/labels/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "folder_c" {
-  description = "The folder ID where the IAM audit configuration was applied for folder C."
-  value       = module.folder_c.audit_config.folder
-}
-
-output "folder_nc" {
-  description = "The folder ID where the IAM audit configuration was applied for folder NC."
-  value       = module.folder_nc.audit_config.folder
-}
