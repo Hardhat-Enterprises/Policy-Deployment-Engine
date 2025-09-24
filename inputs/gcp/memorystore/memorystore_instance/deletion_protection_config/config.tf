e7cacc66a9d0b5111551ba1d7170536f4ca683ff
Guandong <s223930185@deakin.edu.au>
2025-09-24 20:59:06 +0800

    fix bugs: Service: memorystore | Resource: memorystore_instance | Policy: deletion_protection_config Terraform failed to compile!
    Service: memorystore | Resource: memorystore_instance | Policy: authorization_mode
    Terraform failed to compile!

--- inputs/gcp/memorystore/memorystore_instance/deletion_protection_config/config.tf
+++ inputs/gcp/memorystore/memorystore_instance/deletion_protection_config/config.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      # version = ">= 6.15.0"
+      version = "7.3.0"
     }
   }
-
-   # required_version = ">= 1.3.0"
 }
 
 provider "google" {
