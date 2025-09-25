fb10e545270f759a3b7757b14c6a88d345f69b26
Ankita <ankiaanku9777@gmail.com>
2025-09-25 13:16:20 +0530

    removed 256 sha and documented it

--- inputs/gcp/firebase_app_check/android_app/sha256_hashes_presence/c.tf
+++
@@ -1,8 +0,0 @@
-
-resource "google_firebase_android_app" "c" {
-  project    = "ankita-firebase-appcheck-id" 
-  package_name = "com.example.app"
-  display_name = "My Android App"
-  sha1_hashes  = ["0123456789ABCDEF0123456789ABCDEF01234567"]
-  sha256_hashes = ["0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF"]
-}
