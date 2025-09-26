b894da43dbc9751938e51f9183760a4203577ab1
adithya1811 <adithyasubramani.s@gmail.com>
2025-09-26 13:21:24 +1000

    Policies Copied

--- inputs/gcp/Firebase_Data_Connect/data_connect_service/deletion_policy/nc.tf
+++
@@ -1,9 +0,0 @@
-resource "google_firebase_data_connect_service" "nc" {
-  display_name = "firebasedata connect service-nc"
-  project = "my-project-name"
-  location = "us-central1"
-  service_id = "example-service"
-  deletion_policy = "DEFAULT"
-  provider = google-beta
-
-}
