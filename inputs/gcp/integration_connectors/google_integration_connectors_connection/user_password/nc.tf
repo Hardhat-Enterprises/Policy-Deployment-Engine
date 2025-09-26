23cb7c4995fc4d1ecbce34b2a57dd60e351dfe5b
Samuel2875 <s222143244@deakin.edu.au>
2025-09-26 19:17:15 +1000

    New pr for integration_connectors

---
+++ inputs/gcp/integration_connectors/google_integration_connectors_connection/user_password/nc.tf
@@ -0,0 +1,35 @@
+resource "google_secret_manager_secret" "non_compliant_secret" {
+  secret_id = "test-secret"
+  project = "PDE_connectors"
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "non_compliant_secret-version" {
+  secret = google_secret_manager_secret.non_compliant_secret.id
+  secret_data = "dummypassword"
+}
+resource "google_integration_connectors_connection" "non_compliant_zendeskconnection" {
+  name     = "test-zendesk"
+  location = "us-central1"
+  project = "PDE_connectors"  
+  service_account = "compute@developer.gserviceaccount.com"
+  connector_version = "projects/locations/global/providers/zendesk/connectors/zendesk/versions/1"
+
+ auth_config {
+ auth_type = "USER_PASSWORD"
+    user_password {
+      username = "user@xyz.com"
+      password {
+        secret_version = "dummypassword"
+        }
+      }
+    }
+}
+  
+
