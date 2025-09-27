fbc40e371e1020f1cd2084f118a91bf16f31f8fc
adithya1811 <66194732+adithya1811@users.noreply.github.com>
2025-09-27 10:34:01 +1000

    Cloud Bigtable (#227)
    * input copied
    
    * Policies Copied
    
    * update and fixed error

--- inputs/gcp/Firebase_Data_Connect/data_connect_service/deletion_policy/c.tf
+++
@@ -1,8 +0,0 @@
-resource "google_firebase_data_connect_service" "c" {
-  display_name = "firebase data connect service-c"
-  project = "my-project-name"
-  location = "us-central1"
-  service_id = "example-service"
-  deletion_policy = "FORCE"
-  provider = google-beta
-}
