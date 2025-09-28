a206e3c613830d4ee921f0c58bf30dcc1278bc94
Mohamed <ma.bp.afc@outlook.com>
2025-09-29 00:12:25 +1000

    fix(alloydb): package names + policy tweaks + OPA eval cmds

--- inputs/gcp/alloyDB/google_alloydb_backup/continuous_backup_encrypt_enable/c.tf
+++ inputs/gcp/alloyDB/google_alloydb_backup/continuous_backup_encrypt_enable/c.tf
@@ -1,10 +1,24 @@
-resource "google_alloydb_backup" "backup_cmek_on" {
+# resource "google_alloydb_backup" "backup_cmek_on" {
+#   location     = "us-central1"
+#   cluster_name = "projects/p/locations/us-central1/clusters/cluster-c"
+#   project = "123"
+#   backup_id = "backup_cmek_on"
+
+#   encryption_config {
+#     kms_key_name = "projects/p/locations/us-central1/keyRings/r/cryptoKeys/k"
+#   }
+# }
+resource "google_alloydb_backup" "c" {
   location     = "us-central1"
+  backup_id    = "alloydb-backup"
   cluster_name = "projects/p/locations/us-central1/clusters/cluster-c"
   project = "123"
-  backup_id = "backup_cmek_on"
+  type = "ON_DEMAND"
 
-  encryption_config {
-    kms_key_name = "projects/p/locations/us-central1/keyRings/r/cryptoKeys/k"
+  labels = {
+    label = "key"
   }
+
+  
 }
+
