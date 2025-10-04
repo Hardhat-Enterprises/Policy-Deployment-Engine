99d109652a07c30c2353f2d0be2518a9894f30c0
Pat Stuart <patrick.stuart@live.com.au>
2025-10-04 10:11:36 +1000

    try this

--- scripts/auto_test/auto_test.py
+++ scripts/auto_test/auto_test.py
@@ -147,7 +147,7 @@ def run_terraform_commands(input_dir: Path, verbose: bool = False) -> Path | Non
     commands = [
         ["terraform", "init", "-backend=false"],
         ["terraform", "plan", "-input=false", "-out=plan"],
-        ["terraform", "show", "-json", "plan"]
+        ["terraform", "show", "-json", "plan > plan.json"]
     ]
 
     for cmd in commands:
