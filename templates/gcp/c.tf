95c40f7d00d189114a693b9fc8ff13858ca4acc7
patrickandersondeakin <s222531376@deakin.edu.au>
2025-11-23 19:40:34 +1100

    little change to make sure it's actually a template, not my old overwritten file

--- templates/gcp/c.tf
+++ templates/gcp/c.tf
@@ -1,6 +1,6 @@
-resource "google_gemini_data_sharing_with_google_setting" "c1" {
-    data_sharing_with_google_setting_id = "c1"
-    location = "australia-southeast1"
-    enable_preview_data_sharing = false
-    enable_data_sharing = false
+# Describe your resource type here
+# Keep "c" as the name to indicate that this resource and its attributes are compliant
+
+resource "RESOURCE TYPE" "c" {
+  
 }
