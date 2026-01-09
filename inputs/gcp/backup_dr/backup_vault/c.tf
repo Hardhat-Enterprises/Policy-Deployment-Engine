11aa0900b7bb6d7281c7d2f99fa7eb4ba4c3a5a8
Chanaka Roshan Mohotti <romohotti@gmail.com>
2026-01-09 12:55:49 +1100

    Simplify backup vault inputs for region compliance testing
     On branch gcp/service/Backup_and_DR_Service
    Your branch is up to date with 'origin/gcp/service/Backup_and_DR_Service'.
    
    Changes to be committed:
    	modified:   inputs/gcp/backup_dr/backup_vault/c.tf
    	modified:   inputs/gcp/backup_dr/backup_vault/nc.tf

--- inputs/gcp/backup_dr/backup_vault/c.tf
+++ inputs/gcp/backup_dr/backup_vault/c.tf
@@ -1,21 +1,5 @@
 resource "google_backup_dr_backup_vault" "c" {
-  project = "my-gcp-project"
-  location = "australia-southeast1"
-  backup_vault_id    = "backup-vault-test"
-  description = "This is a second backup vault built by Terraform."
-  backup_minimum_enforced_retention_duration = "100000s"
-  annotations = {
-    annotations1 = "bar1"
-    annotations2 = "baz1"
-  }
-  labels = {
-    foo = "bar1"
-    bar = "baz1"
-  }
-  force_update = true
-  access_restriction = "WITHIN_ORGANIZATION"
-  backup_retention_inheritance = "INHERIT_VAULT_RETENTION"
-  ignore_inactive_datasources = true
-  ignore_backup_plan_references = true
-  allow_missing = true
+  project         = "my-gcp-project"
+  location        = "australia-southeast1"
+  backup_vault_id = "c"
 }
