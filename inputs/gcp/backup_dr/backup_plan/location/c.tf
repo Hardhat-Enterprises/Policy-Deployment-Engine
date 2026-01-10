0bb546a4a37242ae5a1ff41b3afc52b2653b2fc0
Chanaka Roshan Mohotti <romohotti@gmail.com>
2026-01-10 23:00:03 +1100

    Fix backup_dr input tests for backup_plan and backup_vault
    modified:   inputs/gcp/backup_dr/backup_plan/location/c.tf
    modified:   inputs/gcp/backup_dr/backup_plan/location/nc.tf
    modified:   inputs/gcp/backup_dr/backup_vault/location/nc.tf

--- inputs/gcp/backup_dr/backup_plan/location/c.tf
+++ inputs/gcp/backup_dr/backup_plan/location/c.tf
@@ -1,4 +1,4 @@
-resource "google_backup_dr_service_config" "c" {
+resource "google_backup_dr_backup_plan" "c" {
   project  = "my-gcp-project"
   location      = "australia-southeast1"
   resource_type = "compute.googleapis.com/Instance"
--- inputs/gcp/backup_dr/backup_plan/location/nc.tf
+++ inputs/gcp/backup_dr/backup_plan/location/nc.tf
@@ -1,4 +1,4 @@
-resource "google_backup_dr_service_config" "nc" {
+resource "google_backup_dr_backup_plan" "nc" {
   project  = "my-gcp-project"
   location      = "us-central1"
   resource_type = "compute.googleapis.com/Instance"
