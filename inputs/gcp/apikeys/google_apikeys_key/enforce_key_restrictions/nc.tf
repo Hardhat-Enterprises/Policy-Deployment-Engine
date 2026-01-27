f5da6170d1639312bb574a0cb947d96984a963ce
trongnhanphan <s223878459@deakin.edu.au>
2026-01-27 21:53:56 +0700

    Revert "add gendoc"
    This reverts commit fbf85fc8ffab403fa2a87427259968e5d85b9da3.

--- inputs/gcp/apikeys/google_apikeys_key/enforce_key_restrictions/nc.tf
+++
@@ -1,6 +0,0 @@
-# Non-compliant example for require_restrictions
-
-resource "google_apikeys_key" "nc" {
-  name         = "apikey-restrictions-non-compliant"
-  display_name = "Non-compliant key (no restrictions)"
-}
