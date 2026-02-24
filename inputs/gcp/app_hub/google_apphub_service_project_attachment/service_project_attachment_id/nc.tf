6441c1f3ff4890f2e9ac076a0fb5b7614a9263b5
shani1116 <0.perera.hishi@gmail.com>
2026-02-24 13:48:37 +1100

    Change incorrect null value in app_hub

--- inputs/gcp/app_hub/google_apphub_service_project_attachment/service_project_attachment_id/nc.tf
+++ inputs/gcp/app_hub/google_apphub_service_project_attachment/service_project_attachment_id/nc.tf
@@ -1,10 +1,5 @@
 resource "google_apphub_service_project_attachment" "nc1" {
   project = "PDE"
-  service_project_attachment_id = "null"
-}
-
-resource "google_apphub_service_project_attachment" "nc2" {
-  project = "PDE"
   service_project_attachment_id = ""
 }
 
