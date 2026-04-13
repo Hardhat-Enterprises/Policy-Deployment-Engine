3d53199ff1403afd9c29d83920de64aaf018ddfe
224774388deakin <s224774388@deakin.edu.au>
2026-04-14 01:22:25 +1000

    Updated Cloud IAM policies - OAuth client and credential location validation

--- inputs/gcp/Cloud_IAM/google_iam_oauth_client_credential/location_validation/c.tf
+++ inputs/gcp/Cloud_IAM/google_iam_oauth_client_credential/location_validation/c.tf
@@ -1,20 +1,18 @@
-resource "google_iam_oauth_client" "client" {
-  oauth_client_id       = "example-client-id"
-  display_name          = "Valid OAuth Client"
-  description           = "Compliant OAuth client"
-  location              = "global"
-  disabled              = false
+resource "google_iam_oauth_client" "client_c" {
+  oauth_client_id = "example-client-id"
+  display_name    = "Valid OAuth Client"
+  location        = "global"
+  project         = "smooth-verve-467716-v1"
   allowed_grant_types   = ["AUTHORIZATION_CODE_GRANT"]
-  allowed_redirect_uris = ["https://www.example.com"]
+  allowed_redirect_uris = ["https://example.com"]
   allowed_scopes        = ["https://www.googleapis.com/auth/cloud-platform"]
   client_type           = "CONFIDENTIAL_CLIENT"
-  project               = "smooth-verve-467716-v1"
 }
 
 resource "google_iam_oauth_client_credential" "c" {
-  oauthclient                = google_iam_oauth_client.client.oauth_client_id
+  oauthclient                = google_iam_oauth_client.client_c.oauth_client_id
   location                   = "global"
-  oauth_client_credential_id = "cred-01a"
-  disabled                   = false
+  oauth_client_credential_id = "cred01"
   display_name               = "Valid Credential"
+  project                    = "smooth-verve-467716-v1"
 }
