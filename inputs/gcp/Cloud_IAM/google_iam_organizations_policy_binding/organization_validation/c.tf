2c8c95ae49e554ef6ebad143e1d3cb473b3eadb0
224774388deakin <s224774388@deakin.edu.au>
2026-04-14 16:08:24 +1000

    Added Cloud IAM organization and OAuth credential validation policies

---
+++ inputs/gcp/Cloud_IAM/google_iam_organizations_policy_binding/organization_validation/c.tf
@@ -0,0 +1,13 @@
+resource "google_iam_organizations_policy_binding" "c" {
+  organization      = "123456789"
+  location          = "global"
+  display_name      = "Compliant Org Policy Binding"
+  policy_kind       = "PRINCIPAL_ACCESS_BOUNDARY"
+  policy_binding_id = "org-binding-c"
+
+  policy = "organizations/123456789/locations/global/principalAccessBoundaryPolicies/my-pab-policy"
+
+  target {
+    principal_set = "//cloudresourcemanager.googleapis.com/organizations/123456789"
+  }
+}
