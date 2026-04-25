1155f30e3551fc987ff4b72741c100911639c4a3
vinhphuc-nguyen <s224325852@gmail.com>
2026-04-25 20:34:36 +1000

    feat: add google_apigee_environment policies and documentation

---
+++ inputs/gcp/apigee/google_apigee_environment/apigee_environment_client_ip_resolution/nc.tf
@@ -0,0 +1,15 @@
+resource "google_apigee_environment" "nc" {
+  name         = "non-compliant-environment"
+  description  = "Apigee Environment"
+  display_name = "non-compliant-environment-1"
+  org_id       = "organizations/pde-org"
+
+  forward_proxy_uri = "ftp://bad-proxy.example.com:8080"
+
+  client_ip_resolution_config {
+    header_index_algorithm {
+      ip_header_name  = "X-Forwarded-For"
+      ip_header_index = 1
+    }
+  }
+}
