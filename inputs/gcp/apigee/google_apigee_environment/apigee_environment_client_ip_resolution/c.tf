b18811abd7ed2447307e7951982d5a24437ae4a4
vinhphuc-nguyen <s224325852@gmail.com>
2026-04-25 21:14:29 +1000

    fix: update c.tf and nc.tf name attribute to match resource identifier

--- inputs/gcp/apigee/google_apigee_environment/apigee_environment_client_ip_resolution/c.tf
+++ inputs/gcp/apigee/google_apigee_environment/apigee_environment_client_ip_resolution/c.tf
@@ -1,11 +1,9 @@
 resource "google_apigee_environment" "c" {
-  name         = "compliant-environment"
+  name         = "c"
   description  = "Apigee Environment"
-  display_name = "compliant-environment-1"
+  display_name = "compliant-environment"
   org_id       = "organizations/pde-org"
-
   forward_proxy_uri = "https://proxy.example.com:8080"
-
   client_ip_resolution_config {
     header_index_algorithm {
       ip_header_name  = "X-Forwarded-For"
--- inputs/gcp/apigee/google_apigee_environment/apigee_environment_client_ip_resolution/nc.tf
+++ inputs/gcp/apigee/google_apigee_environment/apigee_environment_client_ip_resolution/nc.tf
@@ -1,11 +1,9 @@
 resource "google_apigee_environment" "nc" {
-  name         = "non-compliant-environment"
+  name         = "nc"
   description  = "Apigee Environment"
-  display_name = "non-compliant-environment-1"
+  display_name = "non-compliant-environment"
   org_id       = "organizations/pde-org"
-
   forward_proxy_uri = "ftp://bad-proxy.example.com:8080"
-
   client_ip_resolution_config {
     header_index_algorithm {
       ip_header_name  = "X-Forwarded-For"
