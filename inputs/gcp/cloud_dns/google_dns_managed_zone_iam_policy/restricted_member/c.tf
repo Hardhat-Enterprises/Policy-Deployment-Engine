cfc5277e859887f80dd0bfd66557ed9ee64bedc3
KARUNJOT07 <s225616844@deakin.edu.au>
2026-04-27 15:45:32 +1000

    fix: switch to iam_binding for direct role attribute access

--- inputs/gcp/cloud_dns/google_dns_managed_zone_iam_policy/restricted_member/c.tf
+++ inputs/gcp/cloud_dns/google_dns_managed_zone_iam_policy/restricted_member/c.tf
@@ -1,13 +1,10 @@
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/viewer"
-    members = [
-      "user:jane@example.com",
-    ]
-  }
-}
-resource "google_dns_managed_zone_iam_policy" "c" {
+
+
+resource "google_dns_managed_zone_iam_binding" "c" {
   project      = "demo-project"
   managed_zone = "c"
-  policy_data  = data.google_iam_policy.admin.policy_data
+  role         = "roles/viewer"
+  members = [
+    "user:jane@example.com",
+  ]
 }
--- inputs/gcp/cloud_dns/google_dns_managed_zone_iam_policy/restricted_member/nc.tf
+++ inputs/gcp/cloud_dns/google_dns_managed_zone_iam_policy/restricted_member/nc.tf
@@ -1,13 +1,10 @@
-data "google_iam_policy" "admin_public" {
-  binding {
-    role = "roles/owner"
-    members = [
-      "user:jane@example.com",
-    ]
-  }
-}
-resource "google_dns_managed_zone_iam_policy" "nc" {
+
+
+resource "google_dns_managed_zone_iam_binding" "nc" {
   project      = "demo-project"
   managed_zone = "nc"
-  policy_data  = data.google_iam_policy.admin_public.policy_data
+  role         = "roles/owner"
+  members = [
+    "user:jane@example.com",
+  ]
 }
