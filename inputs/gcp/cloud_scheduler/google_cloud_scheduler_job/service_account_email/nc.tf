f96d68260a1a7c48c7f07ff2f592875d4cfd4d84
jamiduo <jaydend1000@live.com.au>
2026-04-29 04:49:33 +1000

    fixed c.tf and nc.tf for service_account_email to pass test

--- inputs/gcp/cloud_scheduler/google_cloud_scheduler_job/service_account_email/nc.tf
+++ inputs/gcp/cloud_scheduler/google_cloud_scheduler_job/service_account_email/nc.tf
@@ -1,4 +1,4 @@
-resource "google_cloud_scheduler_job" "nc_1" {
+resource "google_cloud_scheduler_job" "nc" {
   name        = "nc"
   project     = "PDE"
   description = "test job"
@@ -16,60 +16,4 @@ resource "google_cloud_scheduler_job" "nc_1" {
     } 
 }
 
-resource "google_cloud_scheduler_job" "nc_2" {
-  name        = "nc"
-  project     = "PDE"
-  description = "test job"
-  schedule    = "*/2 * * * *"
-  region      = "australia-southeast1"
-
-  
-  http_target {
-    http_method = "GET"
-    uri         = "https://cloudscheduler.googleapis.com/v1/projects/my-project-name/locations/australia-southeast1/jobs"
-
-    oauth_token {
-      service_account_email = "nc.pde@appspot.gserviceaccount.com"
-    }
-    }
-  
-}
-
-resource "google_cloud_scheduler_job" "nc_3" {
-  name        = "nc"
-  project     = "PDE"
-  description = "test job"
-  schedule    = "*/2 * * * *"
-  region      = "australia-southeast1"
-
-  
-  http_target {
-    http_method = "GET"
-    uri         = "https://cloudscheduler.googleapis.com/v1/projects/my-project-name/locations/australia-southeast1/jobs"
-
-    oidc_token {
-      service_account_email = "nc_pde-compute@developer.gserviceaccount.com"
-    }
-    }
-  
-}
-
-resource "google_cloud_scheduler_job" "nc_4" {
-  name        = "nc"
-  project     = "PDE"
-  description = "test job"
-  schedule    = "*/2 * * * *"
-  region      = "australia-southeast1"
-
-  
-  http_target {
-    http_method = "GET"
-    uri         = "https://cloudscheduler.googleapis.com/v1/projects/my-project-name/locations/australia-southeast1/jobs"
-
-    oidc_token {
-      service_account_email = "nc.pde@appspot.gserviceaccount.com"
-    }
-    }
-  
-}
 
