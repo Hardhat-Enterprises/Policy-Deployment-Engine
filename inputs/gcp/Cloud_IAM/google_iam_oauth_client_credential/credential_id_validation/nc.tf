54462f05a8c074caacf96be6c5f22cf9fab6f0a2
224774388deakin <s224774388@deakin.edu.au>
2026-04-30 14:05:55 +1000

    Cloud IAM

--- inputs/gcp/Cloud_IAM/google_iam_oauth_client_credential/credential_id_validation/nc.tf
+++ inputs/gcp/Cloud_IAM/google_iam_oauth_client_credential/credential_id_validation/nc.tf
@@ -1,7 +1,7 @@
 resource "google_iam_oauth_client_credential" "nc" {
   oauthclient                = "example-client-id"
   location                   = "global"
-  oauth_client_credential_id = "gcp-cred-01a"
+  oauth_client_credential_id = "gcp23424sdresdfscred!@01a"
   disabled                   = false
   display_name               = "Invalid Credential"
   project                    = "smooth-verve-467716-v1"
