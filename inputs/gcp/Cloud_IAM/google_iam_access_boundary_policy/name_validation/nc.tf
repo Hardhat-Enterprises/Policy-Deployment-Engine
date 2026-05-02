0ebd47217f6051aa2d51e250db0bdf7ae7e30482
224774388deakin <s224774388@deakin.edu.au>
2026-05-02 12:36:36 +1000

    Cloud IAM

--- inputs/gcp/Cloud_IAM/google_iam_access_boundary_policy/name_validation/nc.tf
+++ inputs/gcp/Cloud_IAM/google_iam_access_boundary_policy/name_validation/nc.tf
@@ -1,10 +1,14 @@
 resource "google_iam_access_boundary_policy" "nc" {
-  name   = ""          
-  parent = "cloudresourcemanager.googleapis.com/projects/my-project-id"
-  display_name = "My AB policy"
+  parent       = urlencode("cloudresourcemanager.googleapis.com/projects/my-gcp-project")
+  name         = ""
+  display_name = "My Non-Compliant AB policy"
+  
   rules {
     description = "AB rule"
     access_boundary_rule {
+      available_resource    = "*"
+      available_permissions = ["*"]
+      
       availability_condition {
         title      = "Access level expr"
         expression = "request.matchAccessLevels('123456789', ['accessPolicies/123/accessLevels/chromeos_no_lock'])"
