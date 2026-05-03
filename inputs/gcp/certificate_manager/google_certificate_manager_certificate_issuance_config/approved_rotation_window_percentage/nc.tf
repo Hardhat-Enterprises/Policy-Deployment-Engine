91578022d8a22744907c1dd4c43010394e135a1f
Jiafan Li <lijiafan0427@gmail.com>
2026-05-04 00:40:40 +1000

    Add Certificate Manager rotation window policy

---
+++ inputs/gcp/certificate_manager/google_certificate_manager_certificate_issuance_config/approved_rotation_window_percentage/nc.tf
@@ -0,0 +1,15 @@
+resource "google_certificate_manager_certificate_issuance_config" "nc_certificate_issuance_config_rotation" {
+  name        = "nc-certificate-issuance-config-rotation"
+  description = "Non-compliant certificate issuance config rotation window"
+  location    = "us-central1"
+
+  key_algorithm              = "ECDSA_P256"
+  lifetime                   = "1814400s"
+  rotation_window_percentage = 50
+
+  certificate_authority_config {
+    certificate_authority_service_config {
+      ca_pool = "projects/test-project/locations/us-central1/caPools/test-ca-pool"
+    }
+  }
+}
