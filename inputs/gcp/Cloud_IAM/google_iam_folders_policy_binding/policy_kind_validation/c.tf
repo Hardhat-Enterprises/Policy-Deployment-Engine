11e89644f844179d79d8d2a0b0120bf572904b8f
224774388deakin <s224774388@deakin.edu.au>
2026-05-03 21:27:18 +1000

    fix folder policy validation

---
+++ inputs/gcp/Cloud_IAM/google_iam_folders_policy_binding/policy_kind_validation/c.tf
@@ -0,0 +1,13 @@
+resource "google_iam_folders_policy_binding" "c" {
+  folder            = "folders/123456789"
+  location          = "global"
+  display_name      = "valid binding"
+  policy_kind       = "PRINCIPAL_ACCESS_BOUNDARY"
+  policy_binding_id = "c"
+
+  policy = "organizations/123456789/locations/global/principalAccessBoundaryPolicies/policy-1"
+
+  target {
+    principal_set = "//cloudresourcemanager.googleapis.com/folders/123456789"
+  }
+}
---
+++ inputs/gcp/Cloud_IAM/google_iam_folders_policy_binding/policy_kind_validation/nc.tf
@@ -0,0 +1,13 @@
+resource "google_iam_folders_policy_binding" "nc" {
+  folder            = "folders/123456789"
+  location          = "global"
+  display_name      = "invalid binding"
+  policy_kind       = "ACCESS"   # ❌ Not allowed
+  policy_binding_id = "nc"
+
+  policy = "organizations/123456789/locations/global/principalAccessBoundaryPolicies/policy-1"
+
+  target {
+    principal_set = "//cloudresourcemanager.googleapis.com/folders/123456789"
+  }
+}
