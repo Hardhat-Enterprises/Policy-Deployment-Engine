b13ff6cca963cd2046ccc93b63b3dbf040f6fc8b
abhay652 <abhaylakhotra99@gmail.com>
2026-05-06 10:44:47 +0530

    fix: dataset_iam add provider=google-beta per provider_versions guide

--- inputs/gcp/cloud_healthcare/google_healthcare_dataset_iam/role/c.tf
+++ inputs/gcp/cloud_healthcare/google_healthcare_dataset_iam/role/c.tf
@@ -1,5 +1,6 @@
 # Healthcare Dataset IAM - role (compliant)
 # Keep "c" as the name to indicate that this resource and its attributes are compliant
+# Note: This resource is in beta - uses provider = google-beta as per Provider Versions guide
 
 resource "google_healthcare_dataset_iam_member" "c" {
   provider   = google-beta
--- inputs/gcp/cloud_healthcare/google_healthcare_dataset_iam/role/nc.tf
+++ inputs/gcp/cloud_healthcare/google_healthcare_dataset_iam/role/nc.tf
@@ -1,5 +1,6 @@
 # Healthcare Dataset IAM - role (non-compliant)
 # Keep "nc" as the name to indicate that this resource and its attributes are non-compliant
+# Note: This resource is in beta - uses provider = google-beta as per Provider Versions guide
 
 resource "google_healthcare_dataset_iam_member" "nc" {
   provider   = google-beta
