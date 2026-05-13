a082360ee808f9f835cb00be0fc9388e25b70754
Mohamed <ma.bp.afc@outlook.com>
2026-05-13 20:30:41 +1000

    Remove broken Google Endpoints IAM allowed_members policy

--- inputs/gcp/google_endpoints/google_endpoints_service_iam/allowed_members/nc.tf
+++
@@ -1,8 +0,0 @@
-resource "google_endpoints_service_iam_binding" "nc" {
-  service_name = "api.endpoints.my-project-123.cloud.goog"
-  role = "roles/viewer"
-
-  members = [
-    "domain:example.com"
-  ]
-}
