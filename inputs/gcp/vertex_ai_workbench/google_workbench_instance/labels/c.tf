ce60d03942c2fb2e6e38c96330fdb977be92cd0e
BAGWASI <jbagwasi14@gmail.com>
2026-05-16 17:51:01 +1000

    fix: remove labels policy as it is not security related

--- inputs/gcp/vertex_ai_workbench/google_workbench_instance/labels/c.tf
+++
@@ -1,9 +0,0 @@
-resource "google_workbench_instance" "c" {
-  project  = "my-secure-project"
-  name     = "c"
-  location = "australia-southeast2-a"
-  labels = {
-    environment = "dev"
-    team        = "data-science"
-  }
-}
