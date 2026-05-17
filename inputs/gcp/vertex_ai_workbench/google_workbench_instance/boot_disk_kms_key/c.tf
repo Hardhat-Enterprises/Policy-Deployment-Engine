5f9c5e7392b9277dc196376f13f7384a639d3aa5
BAGWASI <92784808+BAGWASI@users.noreply.github.com>
2026-05-17 18:03:11 +1000

    Vertex AI Workbench security policies, inputs, and document… (#395)
    Service completed with policy, inputs and documentation.

---
+++ inputs/gcp/vertex_ai_workbench/google_workbench_instance/boot_disk_kms_key/c.tf
@@ -0,0 +1,10 @@
+resource "google_workbench_instance" "c" {
+  project  = "my-secure-project"
+  name     = "c"
+  location = "australia-southeast2-a"
+  gce_setup {
+    boot_disk {
+      kms_key = "projects/my-secure-project/locations/australia-southeast2/keyRings/my-ring/cryptoKeys/my-key"
+    }
+  }
+}
