0185f7cd53cdabff2c89aa2e39f32cdcf23e2d46
224774388deakin <s224774388@deakin.edu.au>
2026-05-17 13:53:03 +1000

    fix folder policy

--- inputs/gcp/Cloud_IAM/google_iam_oauth_client/client_type_validation/nc.tf
+++ inputs/gcp/Cloud_IAM/google_iam_oauth_client/client_type_validation/nc.tf
@@ -1,12 +1,10 @@
-resource "google_iam_oauth_client" "nc" {
+resource "google_iam_oauth_client" "c" {
   oauth_client_id       = "nc"
-  display_name          = "Invalid OAuth Client"
-  description           = "Non-compliant OAuth client configuration"
   location              = "global"
-  disabled              = false
+
   allowed_grant_types   = ["AUTHORIZATION_CODE_GRANT"]
   allowed_redirect_uris = ["https://www.example.com"]
   allowed_scopes        = ["https://www.googleapis.com/auth/cloud-platform"]
-  client_type           = "INTERNAL_CLIENT"
-  project               = "smooth-verve-467716-v1"
+
+  client_type           = "CINTERNAL_CLIENT"
 }
