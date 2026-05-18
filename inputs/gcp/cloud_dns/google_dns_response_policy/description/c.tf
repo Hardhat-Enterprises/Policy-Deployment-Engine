92bc6adf57815784b3b7efb329985f1c31dc9167
Karunjot Singh <s225616844@deakin.edu.au>
2026-05-18 12:10:46 +1000

    fix: remove description policy as per teacher feedback

--- inputs/gcp/cloud_dns/google_dns_response_policy/description/c.tf
+++
@@ -1,8 +0,0 @@
-resource "google_dns_response_policy" "c" {
-  response_policy_name = "c"
-  description          = "Compliant response policy with description"
-
-  networks {
-    network_url = "projects/demo-project/global/networks/default"
-  }
-}
--- inputs/gcp/cloud_dns/google_dns_response_policy/description/nc.tf
+++
@@ -1,7 +0,0 @@
-resource "google_dns_response_policy" "nc" {
-  response_policy_name = "nc"
-
-  networks {
-    network_url = "projects/demo-project/global/networks/default"
-  }
-}
