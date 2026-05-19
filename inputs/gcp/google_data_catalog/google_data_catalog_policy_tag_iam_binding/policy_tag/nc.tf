0a3dbe7013fba8ede159359db580d123dc8753ca
Runkai Song <justin.song415@qq.com>
2026-05-19 21:19:38 +1000

    Remove all policy_tag policies

--- inputs/gcp/google_data_catalog/google_data_catalog_policy_tag_iam_binding/policy_tag/nc.tf
+++
@@ -1,5 +0,0 @@
-resource "google_data_catalog_policy_tag_iam_binding" "nc" {
-  policy_tag = "projects/other-project-12345/locations/us-central1/taxonomies/other_taxonomy/policyTags/other_policy_tag"
-  role       = "roles/datacatalog.viewer"
-  members    = ["user:security@example.com"]
-}
--- inputs/gcp/google_data_catalog/google_data_catalog_policy_tag_iam_member/policy_tag/nc.tf
+++
@@ -1,5 +0,0 @@
-resource "google_data_catalog_policy_tag_iam_member" "nc" {
-  policy_tag = "projects/other-project-12345/locations/us-central1/taxonomies/other_taxonomy/policyTags/other_policy_tag"
-  role       = "roles/datacatalog.viewer"
-  member     = "user:security@example.com"
-}
