04fd8874909210e08b3a5d95b8b25f96959abd2d
Runkai Song <justin.song415@qq.com>
2026-05-19 20:14:55 +1000

    Remove all project policies

--- inputs/gcp/google_data_catalog/google_data_catalog_entry_group/project/nc.tf
+++
@@ -1,9 +0,0 @@
-resource "google_data_catalog_entry_group" "nc" {
-  entry_group_id = "approved_entry_group"
-
-  display_name = "terraform entry group"
-  description  = "entry group created by Terraform"
-
-  region  = "australia-southeast1"
-  project = "my_project"
-}
