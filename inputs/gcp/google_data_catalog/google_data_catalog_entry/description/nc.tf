9f1f808c50ad7644e11fa1d1a72f3fcff7fd4f7a
Runkai Song <justin.song415@qq.com>
2026-05-19 19:03:52 +1000

    Remove all description policies

--- inputs/gcp/google_data_catalog/google_data_catalog_entry/description/nc.tf
+++
@@ -1,9 +0,0 @@
-resource "google_data_catalog_entry" "nc" {
-  entry_group = "approved_entry_group"
-  entry_id    = "my_entry"
-
-  user_specified_type   = "approved_custom_type"
-  user_specified_system = "SomethingExternal"
-
-  description = ""
-}
--- inputs/gcp/google_data_catalog/google_data_catalog_entry_group/description/nc.tf
+++
@@ -1,9 +0,0 @@
-resource "google_data_catalog_entry_group" "nc" {
-  entry_group_id = "approved_entry_group"
-
-  display_name = "terraform entry group"
-  description  = ""
-
-  region  = "australia-southeast1"
-  project = "my_project"
-}
