230c7f7a2179fdf9d090c758dfb40b3706bcfc63
Runkai Song <justin.song415@qq.com>
2026-05-19 21:00:53 +1000

    Remove parent_policy_tag policy

--- inputs/gcp/google_data_catalog/google_data_catalog_policy_tag/parent_policy_tag/nc.tf
+++
@@ -1,6 +0,0 @@
-resource "google_data_catalog_policy_tag" "nc" {
-  taxonomy          = "projects/gcp-project-12345/locations/australia-southeast1/taxonomies/approved_taxonomy"
-  display_name      = "approved_policy_tag"
-  description       = "Policy tag for approved confidential data."
-  parent_policy_tag = "projects/other-project-12345/locations/us-central1/taxonomies/other_taxonomy/policyTags/other_parent"
-}
