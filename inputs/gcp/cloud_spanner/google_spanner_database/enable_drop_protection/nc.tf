0dc84abd46f1586788d5261d3305e37d809effcd
Vinnakota Anusha <vinnakotaanusha@Vinnakotas-MacBook-Air.local>
2026-05-20 23:55:29 +1000

    Update Cloud Spanner policy examples

--- inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/nc.tf
+++ inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/nc.tf
@@ -1,5 +1,13 @@
 resource "google_spanner_database" "database" {
-  instance               = "my-instance"
-  name                   = "my-database"
+  instance = google_spanner_instance.instance.name
+  name     = "database"
+
   enable_drop_protection = false
+}
+
+resource "google_spanner_instance" "instance" {
+  name         = "test-instance"
+  config       = "regional-australia-southeast1"
+  display_name = "Test Instance"
+  num_nodes    = 1
 }
