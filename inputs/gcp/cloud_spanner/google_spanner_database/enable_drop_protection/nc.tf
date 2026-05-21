323459ee324e9fa5c55d7ebd8a33a29bb93f65b6
Vinnakota Anusha <vinnakotaanusha@Vinnakotas-MacBook-Air.local>
2026-05-21 10:01:07 +1000

    fix: use unique name values in nc.tf

--- inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/nc.tf
+++ inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/nc.tf
@@ -1,13 +1,13 @@
 resource "google_spanner_database" "database_nc" {
   instance = google_spanner_instance.instance_nc.name
-  name     = "database"
+  name     = "database-nc"
 
   enable_drop_protection = false
 }
 
 resource "google_spanner_instance" "instance_nc" {
-  name         = "test-instance"
+  name         = "test-instance-nc"
   config       = "regional-australia-southeast1"
-  display_name = "Test Instance"
+  display_name = "Test Instance NC"
   num_nodes    = 1
 }
