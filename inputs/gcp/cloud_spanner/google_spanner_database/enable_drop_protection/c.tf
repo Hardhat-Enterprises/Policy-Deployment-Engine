b8e2036168a2f612e2f72832084e42f80e856d2c
Vinnakota Anusha <vinnakotaanusha@Vinnakotas-MacBook-Air.local>
2026-05-21 10:09:19 +1000

    fix: rename c.tf resources to follow c/nc naming convention

--- inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/c.tf
+++ inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/c.tf
@@ -1,13 +1,13 @@
-resource "google_spanner_database" "database" {
-  instance = google_spanner_instance.instance.name
-  name     = "database"
+resource "google_spanner_database" "c1" {
+  instance = google_spanner_instance.c1_instance.name
+  name     = "c1"
 
   enable_drop_protection = true
 }
 
-resource "google_spanner_instance" "instance" {
-  name         = "test-instance"
+resource "google_spanner_instance" "c1_instance" {
+  name         = "test-instance-c1"
   config       = "regional-australia-southeast1"
-  display_name = "Test Instance"
+  display_name = "Test Instance C1"
   num_nodes    = 1
 }
