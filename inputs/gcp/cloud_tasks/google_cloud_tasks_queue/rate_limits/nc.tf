65bae52a3ad55f119175c883abe61db858d1ec0e
Vindya Lakshani <s225107484@deakin.edu.au>
2026-05-24 19:44:47 +1000

    Fix rate limits policy input

--- inputs/gcp/cloud_tasks/google_cloud_tasks_queue/rate_limits/nc.tf
+++ inputs/gcp/cloud_tasks/google_cloud_tasks_queue/rate_limits/nc.tf
@@ -4,9 +4,4 @@ resource "google_cloud_tasks_queue" "nc" {
   name     = "nc"
   location = "us-central1"
   project  = "pde-project-vindya"
-
-  rate_limits {
-    max_dispatches_per_second = 1000
-    max_concurrent_dispatches = 100
-  }
 }
