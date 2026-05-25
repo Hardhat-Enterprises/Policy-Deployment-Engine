f0067cea28112494efa0417caf288356d7d382d6
Vinnakota Anusha <s225709739@deakin.edu.au>
2026-05-25 13:00:46 +1000

    Fix resource naming in google_spanner_backup_schedule c.tf

--- inputs/gcp/cloud_spanner/google_spanner_backup_schedule/encryption_config/c.tf
+++ inputs/gcp/cloud_spanner/google_spanner_backup_schedule/encryption_config/c.tf
@@ -1,31 +1,26 @@
-resource "google_spanner_backup_schedule" "c1" {
-  instance         = google_spanner_instance.c1_instance.name
-  database         = google_spanner_database.c1_database.name
-  name             = "c1-backup-schedule"
+resource "google_spanner_backup_schedule" "c" {
+  instance           = google_spanner_instance.c_instance.name
+  database           = google_spanner_database.c_database.name
+  name               = "c-backup-schedule"
   retention_duration = "86400s"
-
   full_backup_spec {}
-
   spec {
     cron_spec {
       text = "0 2 * * *"
     }
   }
-
   encryption_config {
     encryption_type = "CUSTOMER_MANAGED_ENCRYPTION"
     kms_key_name    = "projects/my-project/locations/australia-southeast1/keyRings/my-ring/cryptoKeys/my-key"
   }
 }
-
-resource "google_spanner_instance" "c1_instance" {
-  name         = "test-instance-c1"
+resource "google_spanner_instance" "c_instance" {
+  name         = "test-instance-c"
   config       = "regional-australia-southeast1"
-  display_name = "Test Instance C1"
+  display_name = "Test Instance C"
   num_nodes    = 1
 }
-
-resource "google_spanner_database" "c1_database" {
-  instance = google_spanner_instance.c1_instance.name
-  name     = "c1-database"
+resource "google_spanner_database" "c_database" {
+  instance = google_spanner_instance.c_instance.name
+  name     = "c-database"
 }
