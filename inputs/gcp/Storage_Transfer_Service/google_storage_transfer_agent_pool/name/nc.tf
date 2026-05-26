bf461846504f26f11b3c9c8f44b21ea2b61eeca4
Mohamed <ma.bp.afc@outlook.com>
2026-05-26 17:04:45 +1000

    Fix agent pool name policy check output

--- inputs/gcp/Storage_Transfer_Service/google_storage_transfer_agent_pool/name/nc.tf
+++ inputs/gcp/Storage_Transfer_Service/google_storage_transfer_agent_pool/name/nc.tf
@@ -1,7 +1,6 @@
 resource "google_storage_transfer_agent_pool" "nc" {
-  name         = "goog-agent-pool"
+  name         = "nc"
   display_name = "Non-compliant agent pool"
-
 
   bandwidth_limit {
     limit_mbps = "5000"
