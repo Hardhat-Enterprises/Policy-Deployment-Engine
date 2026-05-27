4405d668437caa2313f955626570a038067843cb
Vinnakota Anusha <s225709739@deakin.edu.au>
2026-05-27 12:14:50 +1000

    Fix enable_drop_protection input resource naming to c1/nc1 convention

--- inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/c.tf
+++ inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/c.tf
@@ -1,12 +1,11 @@
 resource "google_spanner_database" "c1" {
-  instance = google_spanner_instance.c1_instance.name
-  name     = "c1"
-
+  instance               = google_spanner_instance.c1.name
+  name                   = "c1"
   enable_drop_protection = true
 }
 
-resource "google_spanner_instance" "c1_instance" {
-  name         = "test-instance-c1"
+resource "google_spanner_instance" "c1" {
+  name         = "c1"
   config       = "regional-australia-southeast1"
   display_name = "Test Instance C1"
   num_nodes    = 1
--- inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/nc.tf
+++ inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/nc.tf
@@ -1,12 +1,11 @@
 resource "google_spanner_database" "nc1" {
-  instance = google_spanner_instance.nc1_instance.name
-  name     = "nc1"
-
+  instance               = google_spanner_instance.nc1.name
+  name                   = "nc1"
   enable_drop_protection = false
 }
 
-resource "google_spanner_instance" "nc1_instance" {
-  name         = "test-instance-nc1"
+resource "google_spanner_instance" "nc1" {
+  name         = "nc1"
   config       = "regional-australia-southeast1"
   display_name = "Test Instance NC1"
   num_nodes    = 1
