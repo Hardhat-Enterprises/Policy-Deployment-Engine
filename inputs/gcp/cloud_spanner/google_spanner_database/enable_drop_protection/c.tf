df86ac91f11ab4040fac3c1d9e52bb0de5123b89
anushav12345 <s225709739@deakin.edu.au>
2026-05-30 16:01:40 +1000

    Update c.tf

--- inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/c.tf
+++ inputs/gcp/cloud_spanner/google_spanner_database/enable_drop_protection/c.tf
@@ -1,11 +1,11 @@
 resource "google_spanner_database" "c" {
   instance               = google_spanner_instance.c.name
-  name                   = "c"
+  name                   = "c-database"
   enable_drop_protection = true
 }
 
 resource "google_spanner_instance" "c" {
-  name         = "c"
+  name         = "c-instance"
   config       = "regional-australia-southeast1"
   display_name = "Test Instance C"
   num_nodes    = 1
