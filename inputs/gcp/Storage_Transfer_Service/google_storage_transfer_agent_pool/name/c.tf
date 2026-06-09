d22c4ccc42527963e715764ce770a6558f0b2258
Mohamed <ma.bp.afc@outlook.com>
2026-06-09 19:11:43 +1000

    Address Storage Transfer Service review feedback

--- inputs/gcp/Storage_Transfer_Service/google_storage_transfer_agent_pool/name/c.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_transfer_agent_pool" "c" {
-  name         = "agent-pool-example"
-  display_name = "Compliant agent pool"
-
-  bandwidth_limit {
-    limit_mbps = "250"
-  }
-}
--- inputs/gcp/Storage_Transfer_Service/google_storage_transfer_agent_pool/name/nc.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_transfer_agent_pool" "nc" {
-  name         = "nc"
-  display_name = "Non-compliant agent pool"
-
-  bandwidth_limit {
-    limit_mbps = "5000"
-  }
-}
