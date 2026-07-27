90ce9afa9ebb00cf0cebc5fae441ed0038d5796a
devv000 <devkalansuriya@gmail.com>
2026-07-27 18:31:21 +1000

    Add location policy for IAM workforce pool provider

---
+++ inputs/gcp/Cloud IAM/google_iam_workforce_pool_provider/location/compliant.tf
@@ -0,0 +1,14 @@
+resource "google_iam_workforce_pool_provider" "compliant_example_1" {
+  workforce_pool_id = "example-workforce-pool"
+  provider_id       = "location-provider-good"
+  location          = "global"
+
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+  }
+
+  oidc {
+    issuer_uri = "https://accounts.example.com"
+    client_id  = "example-client-id"
+  }
+}
