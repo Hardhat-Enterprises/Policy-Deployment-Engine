e9a2a81cc4202f1c1d0c2440675c3bbebb6a772d
Methusha <methuthurai@gmail.com>
2026-08-02 20:53:13 +1000

    Update google_ces_agent test inputs for instruction and after_agent_callback.disabled

---
+++ inputs/gcp/Customer Engagement Suite/google_ces_agent/instruction/nonCompliant.tf
@@ -0,0 +1,8 @@
+resource "google_ces_agent" "non_compliant_example_1" {
+  agent_id     = "agent-1"
+  display_name = "example-agent"
+  app          = "fake-app"
+  location     = "australia-southeast1"
+
+  instruction = ""
+}
