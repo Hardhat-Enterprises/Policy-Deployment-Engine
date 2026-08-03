1a98bbed33fdd7a0e33010dc6e0f79e9ee8eca0b
Methusha <methuthurai@gmail.com>
2026-08-03 13:13:30 +1000

    Created location and deletion_policy policy files and var file

--- inputs/gcp/Customer Engagement Suite/google_ces_example/location/nonCompliant.tf
+++ inputs/gcp/Customer Engagement Suite/google_ces_example/location/nonCompliant.tf
@@ -1,5 +1,5 @@
 resource "google_ces_example" "non_compliant_example_1" {
-    location     = "australia-southeast1"
+    location     = "us-east1"
     display_name = "example-name"
     app          = "example-app"
     example_id   = "example-id"
