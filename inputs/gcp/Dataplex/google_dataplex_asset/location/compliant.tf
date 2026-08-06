e64b9e3752afc6f90eeaf6b065bdc98ea5b4502b
Rom Fridman <s222254964@deakin.edu.au>
2026-08-06 17:22:14 +1000

    Add google_dataplex_asset security policies

--- inputs/gcp/Dataplex/google_dataplex_asset/location/compliant.tf
+++ inputs/gcp/Dataplex/google_dataplex_asset/location/compliant.tf
@@ -8,7 +8,7 @@ resource "google_dataplex_asset" "compliant_example_1" {
   dataplex_zone = "google_dataplex_zone.basic_zone.name"
 
   discovery_spec {
-    enabled = false
+    enabled = true
   }
 
   resource_spec {
