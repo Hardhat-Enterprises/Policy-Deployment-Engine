700464fcf8631c0c1c40a953a2ce831c65464f55
Garry Zhuang <garryzhuang1@gmail.com>
2026-08-17 02:12:38 +1000

    fix: replaced resource reference to a string

--- inputs/gcp/Compute Engine/google_compute_instant_snapshot/zone/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_instant_snapshot/zone/nonCompliant.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instant_snapshot" "non_compliant_example_1" {
   name         = "non_compliant_example_1"
   zone         = "us-central1-a"
-  source_disk  = google_compute_disk.foo_non_compliant_1.self_link
+  source_disk  = "projects/fake-project/zones/us-central1-a/disks/fake-disk"
 }
