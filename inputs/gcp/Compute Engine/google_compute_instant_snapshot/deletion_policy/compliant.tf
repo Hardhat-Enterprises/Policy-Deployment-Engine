6a7baacb5a1ba46e29e85d23018df55b43e1620f
Garry Zhuang <garryzhuang1@gmail.com>
2026-08-17 02:06:49 +1000

    fix: removed extra dependancies and replaced with fake values

--- inputs/gcp/Compute Engine/google_compute_instant_snapshot/deletion_policy/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_instant_snapshot/deletion_policy/compliant.tf
@@ -1,9 +1,3 @@
-resource "google_compute_disk" "foo_compliant_1" {
-  name = "example-disk"
-  type = "pd-ssd"
-  size = 10
-}
-
 resource "google_compute_instant_snapshot" "compliant_example_1" {
   name         = "compliant_example_1"
   zone         = "us-central1-a"
--- inputs/gcp/Compute Engine/google_compute_instant_snapshot/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_instant_snapshot/deletion_policy/nonCompliant.tf
@@ -1,9 +1,3 @@
-resource "google_compute_disk" "foo_non_compliant_1" {
-  name = "example-disk"
-  type = "pd-ssd"
-  size = 10
-}
-
 resource "google_compute_instant_snapshot" "non_compliant_example_1" {
   name         = "non_compliant_example_1"
   zone         = "us-central1-a"
