0860e6090716ea5887674cb523b31119dc85dbeb
Garry Zhuang <garryzhuang1@gmail.com>
2026-08-17 01:53:33 +1000

    fix: typo and missing referneces

--- inputs/gcp/Compute Engine/google_compute_instant_snapshot/deletion_policy/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_instant_snapshot/deletion_policy/compliant.tf
@@ -1,4 +1,10 @@
-resource "google_compute_instant_snapshot" ""compliant_example_1" {
+resource "google_compute_disk" "foo" {
+  name = "example-disk"
+  type = "pd-ssd"
+  size = 10
+}
+
+resource "google_compute_instant_snapshot" "compliant_example_1" {
   name         = "compliant_example_1"
   zone         = "us-central1-a"
   source_disk  = google_compute_disk.foo.self_link
--- inputs/gcp/Compute Engine/google_compute_instant_snapshot/zone/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_instant_snapshot/zone/compliant.tf
@@ -1,3 +1,9 @@
+resource "google_compute_disk" "foo" {
+  name = "example-disk"
+  type = "pd-ssd"
+  size = 10
+}
+
 resource "google_compute_instant_snapshot" "non_compliant_example_1" {
   name         = "non_compliant_example_1"
   zone         = "australia-southeast1-a"
