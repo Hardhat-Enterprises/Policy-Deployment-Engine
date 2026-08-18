d014c2cf754d5f08fce361f512954035ecfdec93
Navindra-cell <senonperera07@gmail.com>
2026-08-18 13:02:07 +1000

    Update compliant.tf

--- inputs/gcp/BigQuery/google_bigquery_routine/remote_function_options.endpoint/compliant.tf
+++ inputs/gcp/BigQuery/google_bigquery_routine/remote_function_options.endpoint/compliant.tf
@@ -1,18 +1,18 @@
 resource "google_bigquery_routine" "compliant_example_1" {
-  project       = "your-project-id"
-  dataset_id    = "compliant_example_1"
-  routine_id    = "your_routine_name"
-  routine_type  = "SCALAR_FUNCTION"
+  project         = "your-project-id"
+  dataset_id      = "compliant_example_1"
+  routine_id      = "your_routine_name"
+  routine_type    = "SCALAR_FUNCTION"
   definition_body = "x * 2"
-  language      = "SQL"
-  data_governance_type = "DATA_MASKING"
+  language        = "SQL"
 
-    remote_function_options {
-    endpoint = "https://australia-southeast1-my_gcf_project.cloudfunctions.net/remote_add"
-    connection = "google_bigquery_connection.test.name"
+  remote_function_options {
+    endpoint          = "https://australia-southeast1-my_gcf_project.cloudfunctions.net/remote_add"
+    connection        = "google_bigquery_connection.test.name"
     max_batching_rows = "10"
+
     user_defined_context = {
-      "z": "1.5",
+      "z" = "1.5"
     }
   }
 }
