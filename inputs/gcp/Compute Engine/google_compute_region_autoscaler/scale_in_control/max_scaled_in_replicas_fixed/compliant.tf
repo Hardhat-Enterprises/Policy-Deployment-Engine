78d9525afc2aad81e8262e47ce0830b6784e9c40
Ashi-Athu <ashiathu10@gmail.com>
2026-08-18 12:00:23 +1000

    Create compliant.tf

---
+++ inputs/gcp/Compute Engine/google_compute_region_autoscaler/scale_in_control/max_scaled_in_replicas_fixed/compliant.tf
@@ -0,0 +1,19 @@
+resource "google_compute_region_autoscaler" "compliant_example_1" {
+  name             = "compliant-autoscaler"
+  project          = "my-approved-project"
+  region           = "us-central1"
+  target           = "https://www.googleapis.com/compute/v1/projects/my-project/regions/us-central1/instanceGroupManagers/my-group"
+
+  autoscaling_policy {
+    max_replicas = 5
+    min_replicas = 1
+    scale_in_control {
+      max_scaled_in_replicas {
+        fixed = 2
+      }
+    }
+    cpu_utilization {
+      target = 0.6
+    }
+  }
+}
