c16c5974e9ba7b612ed4334fda203b8acb13bea8
Navindra-cell <senonperera07@gmail.com>
2026-08-18 13:07:44 +1000

    Create compliant.tf

---
+++ inputs/gcp/BigQuery/google_bigquery_routine/deletion_policy/compliant.tf
@@ -0,0 +1,10 @@
+resource "google_bigquery_routine" "compliant_example_1" {
+  project         = "your-project-id"
+  dataset_id      = "compliant_example_1"
+  routine_id      = "your_routine_name"
+  routine_type    = "SCALAR_FUNCTION"
+  definition_body = "x * 2"
+  language        = "SQL"
+
+  deletion_policy = "PREVENT"
+}
