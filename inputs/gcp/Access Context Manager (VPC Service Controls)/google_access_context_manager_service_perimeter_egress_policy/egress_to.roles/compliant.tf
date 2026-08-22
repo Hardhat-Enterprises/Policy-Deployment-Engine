9234bc26d1362e5732b8b1748be2b8337f344fd1
sarthakghildiyal <sarthakghildiyal@gmail.com>
2026-08-22 19:01:18 +1000

    Add service perimeter egress policies

---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter_egress_policy/egress_to.roles/compliant.tf
@@ -0,0 +1,13 @@
+resource "google_access_context_manager_service_perimeter_egress_policy" "compliant_example_1" {
+  perimeter = "accessPolicies/123456/servicePerimeters/my_perimeter"
+  title     = "allow-approved-egress-role"
+
+  egress_from {
+    identity_type = "ANY_SERVICE_ACCOUNT"
+  }
+
+  egress_to {
+    resources = ["projects/123456789"]
+    roles     = ["roles/bigquery.dataViewer"]
+  }
+}
---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter_egress_policy/egress_to.roles/nonCompliant.tf
@@ -0,0 +1,13 @@
+resource "google_access_context_manager_service_perimeter_egress_policy" "non_compliant_example_1" {
+  perimeter = "accessPolicies/123456/servicePerimeters/my_perimeter"
+  title     = "allow-admin-egress-role"
+
+  egress_from {
+    identity_type = "ANY_SERVICE_ACCOUNT"
+  }
+
+  egress_to {
+    resources = ["projects/123456789"]
+    roles     = ["roles/owner"]
+  }
+}
