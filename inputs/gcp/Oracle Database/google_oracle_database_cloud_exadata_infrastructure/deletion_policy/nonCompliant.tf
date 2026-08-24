43208702f737e2f532d52b333f1fc48d0aede9e4
ShreyashD1317 <shreyash2612@gmail.com>
2026-08-25 09:48:51 +1000

    fix(terraform): update Exadata infrastructure ID and location in non-compliant configuration

--- inputs/gcp/Oracle Database/google_oracle_database_cloud_exadata_infrastructure/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Oracle Database/google_oracle_database_cloud_exadata_infrastructure/deletion_policy/nonCompliant.tf
@@ -1,6 +1,5 @@
 resource "google_oracle_database_cloud_exadata_infrastructure" "non_compliant_example_1" {
   cloud_exadata_infrastructure_id = "non-compliant-example-1"
-  display_name                    = "non-compliant-example-1"
   location                        = "australia-southeast2"
   deletion_policy                 = "DELETE"
 
