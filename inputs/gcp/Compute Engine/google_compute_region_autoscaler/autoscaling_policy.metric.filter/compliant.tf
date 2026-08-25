cc3357f77cf61b0df7b0b441bd93f9b28c2a35e1
Ashi-Athu <ashiathu10@gmail.com>
2026-08-25 14:00:00 +1000

    Create compliant.tf

---
+++ inputs/gcp/Compute Engine/google_compute_region_autoscaler/autoscaling_policy.metric.filter/compliant.tf
@@ -0,0 +1,16 @@
+resource "google_compute_region_autoscaler" "compliant_example_1" {
+  name             = "compliant-autoscaler"
+  project          = "my-approved-project"
+  region           = "us-central1"
+  target           = "https://www.googleapis.com/compute/v1/projects/my-project/regions/us-central1/instanceGroupManagers/my-group"
+
+  autoscaling_policy {
+    max_replicas = 5
+    min_replicas = 1
+    metric {
+      name   = "custom.googleapis.com/requests_per_instance"
+      filter = "resource.type = \"gce_instance\" AND metric.labels.instance_name = \"web-server\""
+      target = 100
+    }
+  }
+}
