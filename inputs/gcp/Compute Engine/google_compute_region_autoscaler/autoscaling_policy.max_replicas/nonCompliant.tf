6b2138addb9ad5268556739a112b6b1f2101291d
Ashi-Athu <ashiathu10@gmail.com>
2026-08-25 19:09:34 +1000

    Update nonCompliant.tf

--- inputs/gcp/Compute Engine/google_compute_region_autoscaler/autoscaling_policy.max_replicas/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_region_autoscaler/autoscaling_policy.max_replicas/nonCompliant.tf
@@ -3,6 +3,7 @@ resource "google_compute_region_autoscaler" "non_compliant_example_1" {
   project          = "my-approved-project"
   region           = "us-central1"
   target           = "https://www.googleapis.com/compute/v1/projects/my-project/regions/us-central1/instanceGroupManagers/my-group"
+  description      = "non_compliant_example_1"
 
   autoscaling_policy {
     max_replicas = 1000
