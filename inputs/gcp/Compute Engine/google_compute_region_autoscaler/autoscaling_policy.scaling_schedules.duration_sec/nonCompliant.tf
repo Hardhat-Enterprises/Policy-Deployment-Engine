a00c84df7cc70ad81bf003bb00466a583f47fd46
Ashi-Athu <ashiathu10@gmail.com>
2026-08-25 14:31:37 +1000

    Create nonCompliant.tf

---
+++ inputs/gcp/Compute Engine/google_compute_region_autoscaler/autoscaling_policy.scaling_schedules.duration_sec/nonCompliant.tf
@@ -0,0 +1,22 @@
+resource "google_compute_region_autoscaler" "non_compliant_example_1" {
+  name             = "non-compliant-autoscaler"
+  project          = "my-approved-project"
+  region           = "us-central1"
+  target           = "https://www.googleapis.com/compute/v1/projects/my-project/regions/us-central1/instanceGroupManagers/my-group"
+
+  autoscaling_policy {
+    max_replicas = 5
+    min_replicas = 1
+    cpu_utilization {
+      target = 0.6
+    }
+    scaling_schedules {
+      name                   = "weekday-morning-peak"
+      disabled               = false
+      duration_sec           = 300
+      min_required_replicas  = 3
+      schedule               = "0 9 * * 1-5"
+      time_zone              = "Australia/Melbourne"
+    }
+  }
+}
