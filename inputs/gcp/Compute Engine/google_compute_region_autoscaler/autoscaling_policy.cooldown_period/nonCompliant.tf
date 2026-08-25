03727b8370f496b133086ab4a6896345b7ba3c45
Ashi-Athu <ashiathu10@gmail.com>
2026-08-25 13:21:30 +1000

    Create nonCompliant.tf
    add autoscaling_policy.cooldown_period nonCompliant.tf

---
+++ inputs/gcp/Compute Engine/google_compute_region_autoscaler/autoscaling_policy.cooldown_period/nonCompliant.tf
@@ -0,0 +1,15 @@
+resource "google_compute_region_autoscaler" "non_compliant_example_1" {
+  name             = "non-compliant-autoscaler"
+  project          = "my-approved-project"
+  region           = "us-central1"
+  target           = "https://www.googleapis.com/compute/v1/projects/my-project/regions/us-central1/instanceGroupManagers/my-group"
+
+  autoscaling_policy {
+    max_replicas    = 5
+    min_replicas    = 1
+    cooldown_period  = 5
+    cpu_utilization {
+      target = 0.6
+    }
+  }
+}
