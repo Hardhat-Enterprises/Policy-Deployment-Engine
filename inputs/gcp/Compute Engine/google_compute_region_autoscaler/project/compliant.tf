04a29cbb661acf7a4e154ca71d53875e69d8aab0
Ashi-Athu <ashiathu10@gmail.com>
2026-08-25 19:05:31 +1000

    Update compliant.tf

--- inputs/gcp/Compute Engine/google_compute_region_autoscaler/project/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_region_autoscaler/project/compliant.tf
@@ -3,6 +3,7 @@ resource "google_compute_region_autoscaler" "compliant_example_1" {
   project          = "my-approved-project"
   region           = "us-central1"
   target           = "https://www.googleapis.com/compute/v1/projects/my-project/regions/us-central1/instanceGroupManagers/my-group"
+  description      = "compliant_example_1"
 
   autoscaling_policy {
     max_replicas = 5
