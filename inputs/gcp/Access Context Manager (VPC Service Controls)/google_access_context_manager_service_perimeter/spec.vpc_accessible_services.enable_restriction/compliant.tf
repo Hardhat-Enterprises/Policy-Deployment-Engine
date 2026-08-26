5235bc8b37ce7d66f654b38951fafd8125e90c59
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-26 23:41:40 +1000

    Create compliant.tf

---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.vpc_accessible_services.enable_restriction/compliant.tf
@@ -0,0 +1,13 @@
+resource "google_access_context_manager_service_perimeter" "example" {
+  parent = "accessPolicies/123456789"
+  name   = "accessPolicies/123456789/servicePerimeters/example"
+  title  = "example"
+
+  spec {
+    vpc_accessible_services {
+      enable_restriction = true
+    }
+  }
+
+  use_explicit_dry_run_spec = true
+}
