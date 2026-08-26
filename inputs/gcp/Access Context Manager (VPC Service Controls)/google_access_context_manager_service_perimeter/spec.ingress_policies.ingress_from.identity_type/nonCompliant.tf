064e06b26852dfc8df03879ec31ece5716ed61a7
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-27 00:41:04 +1000

    Update nonCompliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.ingress_policies.ingress_from.identity_type/nonCompliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.ingress_policies.ingress_from.identity_type/nonCompliant.tf
@@ -1,20 +1,19 @@
-package terraform.gcp.security.access_context_manager_vpc_service_controls.google_access_context_manager_service_perimeter.spec_egress_policies_egress_from_identity_type
+resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
+  parent = "accessPolicies/123456789"
+  name   = "noncompliant_ingress_identities"
+  title  = "noncompliant_ingress_identities"
 
-import data.terraform.helpers as helpers
-import data.terraform.gcp.security.access_context_manager_vpc_service_controls.google_access_context_manager_service_perimeter.vars as vars
+  spec {
+    restricted_services = ["storage.googleapis.com"]
 
-conditions := [[
-    {
-        "situation_description": "Egress access should restrict the types of identities allowed outside the Service Perimeter.",
-        "remedies": ["Configure spec.egress_policies.egress_from.identity_type to an approved identity type."],
-    },
-    {
-        "condition": "Egress identity type must be restricted.",
-        "attribute_path": ["spec", "egress_policies", "egress_from", "identity_type"],
-        "values": ["ANY_SERVICE_ACCOUNT"],
-        "policy_type": "whitelist",
-    },
-]]
+    ingress_policies {
+      ingress_from {
+        identities = [
+          "allUsers"
+        ]
+      }
+    }
+  }
 
-message := helpers.get_multi_summary(conditions, vars.variables).message
-details := helpers.get_multi_summary(conditions, vars.variables).details
+  use_explicit_dry_run_spec = true
+}
