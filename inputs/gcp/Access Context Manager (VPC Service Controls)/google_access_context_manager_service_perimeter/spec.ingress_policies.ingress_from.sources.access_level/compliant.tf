fdad261d1174600aee576978e278414f7e0aec6f
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-26 23:26:46 +1000

    Create compliant.tf

---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.ingress_policies.ingress_from.sources.access_level/compliant.tf
@@ -0,0 +1,17 @@
+resource "google_access_context_manager_service_perimeter" "compliant_example_1" {
+  parent = "accessPolicies/123456789"
+  name   = "compliant_ingress_access_level"
+  title  = "compliant_ingress_access_level"
+
+  spec {
+    ingress_policies {
+      ingress_from {
+        sources {
+          access_level = "accessPolicies/123456789/accessLevels/restricted_access"
+        }
+      }
+    }
+  }
+
+  use_explicit_dry_run_spec = true
+}
