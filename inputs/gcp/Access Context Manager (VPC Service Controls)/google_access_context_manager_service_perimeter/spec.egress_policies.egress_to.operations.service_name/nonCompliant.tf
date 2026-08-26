7d1f817bb5b2fdd8c9de62734c405f864be8fdf9
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-26 23:11:38 +1000

    Create nonCompliant.tf

---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.egress_policies.egress_to.operations.service_name/nonCompliant.tf
@@ -0,0 +1,17 @@
+resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
+  parent = "accessPolicies/123456789"
+  name   = "noncompliant_egress_service_name"
+  title  = "noncompliant_egress_service_name"
+
+  spec {
+    egress_policies {
+      egress_to {
+        operations {
+          service_name = "*"
+        }
+      }
+    }
+  }
+
+  use_explicit_dry_run_spec = true
+}
