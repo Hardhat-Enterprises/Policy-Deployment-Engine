b3e2c2e41b9548ea6058bcb7a5904a57a8307e9c
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-27 00:32:21 +1000

    Update compliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.egress_policies.egress_to.operations.method_selectors.permission/compliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.egress_policies.egress_to.operations.method_selectors.permission/compliant.tf
@@ -4,6 +4,8 @@ resource "google_access_context_manager_service_perimeter" "compliant_example_1"
   title  = "compliant_egress_permission"
 
   spec {
+    restricted_services = ["storage.googleapis.com"]
+
     egress_policies {
       egress_to {
         operations {
