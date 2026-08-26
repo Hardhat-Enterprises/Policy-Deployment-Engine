3ae3123e30ccddf75970861b79763c3a158ac9d4
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-26 23:22:01 +1000

    Create nonCompliant.tf

---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.ingress_policies.ingress_from.identities/nonCompliant.tf
@@ -0,0 +1,17 @@
+resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
+  parent = "accessPolicies/123456789"
+  name   = "noncompliant_ingress_identities"
+  title  = "noncompliant_ingress_identities"
+
+  spec {
+    ingress_policies {
+      ingress_from {
+        identities = [
+          "allUsers"
+        ]
+      }
+    }
+  }
+
+  use_explicit_dry_run_spec = true
+}
