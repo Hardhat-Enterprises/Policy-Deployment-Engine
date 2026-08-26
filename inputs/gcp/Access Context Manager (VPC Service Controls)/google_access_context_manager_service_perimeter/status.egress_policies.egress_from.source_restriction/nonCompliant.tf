a3ccdc7fbc6dea0693104ac4d48fb9e9c0079568
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-27 00:21:56 +1000

    Update nonCompliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/status.egress_policies.egress_from.source_restriction/nonCompliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/status.egress_policies.egress_from.source_restriction/nonCompliant.tf
@@ -1,9 +1,11 @@
 resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
   parent = "accessPolicies/123456789"
-  name   = "noncompliant_status_egress_source_restriction"
+  name   = "accessPolicies/123456789/servicePerimeters/noncompliant_status_egress_source_restriction"
   title  = "noncompliant_status_egress_source_restriction"
 
   status {
+    restricted_services = ["storage.googleapis.com"]
+
     egress_policies {
       egress_from {
         source_restriction = "SOURCE_RESTRICTION_DISABLED"
