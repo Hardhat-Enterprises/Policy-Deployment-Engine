c0e214f9992bbf6d0e9e6bbef3f758e17fa8a57e
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-27 00:40:12 +1000

    Update nonCompliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.ingress_policies.ingress_from.identity_type/nonCompliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.ingress_policies.ingress_from.identity_type/nonCompliant.tf
@@ -1,19 +1,19 @@
-package terraform.gcp.security.access_context_manager_vpc_service_controls.google_access_context_manager_service_perimeter.spec_egress_policies_egress_from_identities
+package terraform.gcp.security.access_context_manager_vpc_service_controls.google_access_context_manager_service_perimeter.spec_egress_policies_egress_from_identity_type
 
 import data.terraform.helpers as helpers
 import data.terraform.gcp.security.access_context_manager_vpc_service_controls.google_access_context_manager_service_perimeter.vars as vars
 
 conditions := [[
-  {
-    "situation_description": "Egress access should be restricted to explicitly approved identities.",
-    "remedies": ["Configure spec.egress_policies.egress_from.identities with approved user or service account identities."],
-  },
-  {
-    "condition": "Egress identities must be explicitly approved.",
-    "attribute_path": ["spec", "egress_policies", "egress_from", "identities"],
-    "values": ["serviceAccount:approved@example.iam.gserviceaccount.com"],
-    "policy_type": "whitelist",
-  },
+    {
+        "situation_description": "Egress access should restrict the types of identities allowed outside the Service Perimeter.",
+        "remedies": ["Configure spec.egress_policies.egress_from.identity_type to an approved identity type."],
+    },
+    {
+        "condition": "Egress identity type must be restricted.",
+        "attribute_path": ["spec", "egress_policies", "egress_from", "identity_type"],
+        "values": ["ANY_SERVICE_ACCOUNT"],
+        "policy_type": "whitelist",
+    },
 ]]
 
 message := helpers.get_multi_summary(conditions, vars.variables).message
