7a62437b217f8dee0f0bc5aaf94080ab99af1c43
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-26 23:46:44 +1000

    Update compliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.vpc_accessible_services.enable_restriction/compliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.vpc_accessible_services.enable_restriction/compliant.tf
@@ -1,4 +1,4 @@
-resource "google_access_context_manager_service_perimeter" "example" {
+resource "google_access_context_manager_service_perimeter" "compliant_example_1" {
   parent = "accessPolicies/123456789"
   name   = "accessPolicies/123456789/servicePerimeters/example"
   title  = "example"
