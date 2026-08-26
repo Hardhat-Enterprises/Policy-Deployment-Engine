56c5177354ef08ec7b2ab0dcca0ce55617a3c274
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-27 00:25:21 +1000

    Update compliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/status.ingress_policies.ingress_from.identity_type/compliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/status.ingress_policies.ingress_from.identity_type/compliant.tf
@@ -1,9 +1,11 @@
 resource "google_access_context_manager_service_perimeter" "compliant_example_1" {
   parent = "accessPolicies/123456789"
-  name   = "accessPolicies/123456789/servicePerimeters/example"
-  title  = "example"
+  name   = "accessPolicies/123456789/servicePerimeters/compliant_status_ingress_identity_type"
+  title  = "compliant_status_ingress_identity_type"
 
   status {
+    restricted_services = ["storage.googleapis.com"]
+
     ingress_policies {
       ingress_from {
         identity_type = "ANY_SERVICE_ACCOUNT"
