97b79d1b94c6a9c896f0313b490dc4b803944af5
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-27 00:06:30 +1000

    Create nonCompliant.tf

---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/status.vpc_accessible_services.enable_restriction/nonCompliant.tf
@@ -0,0 +1,11 @@
+resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
+  parent = "accessPolicies/123456789"
+  name   = "accessPolicies/123456789/servicePerimeters/example"
+  title  = "example"
+
+  status {
+    vpc_accessible_services {
+      enable_restriction = false
+    }
+  }
+}
