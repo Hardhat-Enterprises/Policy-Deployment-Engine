96b5ba94e40546249e938deabea98815f0d5e796
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-26 22:47:07 +1000

    Create compliant.tf

---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.egress_policies.egress_from.identities/compliant.tf
@@ -0,0 +1,15 @@
+resource "google_access_context_manager_service_perimeter" "compliant_example_1" {
+  parent = "accessPolicies/123456789"
+  name   = "compliant_egress_identities"
+  title  = "compliant_egress_identities"
+
+  spec {
+    egress_policies {
+      egress_from {
+        identities = ["serviceAccount:approved@example.iam.gserviceaccount.com"]
+      }
+    }
+  }
+
+  use_explicit_dry_run_spec = true
+}
