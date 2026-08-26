c7524bd3858e08cf9593d2a6975cdaad2a4befa3
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-26 23:24:22 +1000

    Create compliant.tf

---
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.ingress_policies.ingress_from.identity_type/compliant.tf
@@ -0,0 +1,15 @@
+resource "google_access_context_manager_service_perimeter" "compliant_example_1" {
+  parent = "accessPolicies/123456789"
+  name   = "compliant_ingress_identity_type"
+  title  = "compliant_ingress_identity_type"
+
+  spec {
+    ingress_policies {
+      ingress_from {
+        identity_type = "ANY_SERVICE_ACCOUNT"
+      }
+    }
+  }
+
+  use_explicit_dry_run_spec = true
+}
