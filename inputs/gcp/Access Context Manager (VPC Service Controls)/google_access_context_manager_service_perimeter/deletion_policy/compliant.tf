5754b2cd4bf5d6ff16f6864d107358d7516599b4
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-26 14:33:22 +1000

    Update compliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/deletion_policy/compliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/deletion_policy/compliant.tf
@@ -1,4 +1,4 @@
-resource "google_access_context_manager_service_perimeter" "compliant_deletion_policy" {
+resource "google_access_context_manager_service_perimeter" "compliant_example_1" {
   parent = "accessPolicies/123456789"
   name   = "compliant_deletion_policy"
   title  = "protected_perimeter"
