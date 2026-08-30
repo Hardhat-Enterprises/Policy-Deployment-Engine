cfc1e2f0ef696175f4cd2b218929aa60d1d042ce
Hongbin Zeng <cenghongbin31@gmail.com>
2026-08-30 17:58:40 +1000

    Add FIX

--- inputs/gcp/Vertex AI/google_vertex_ai_index/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Vertex AI/google_vertex_ai_index/deletion_policy/nonCompliant.tf
@@ -2,19 +2,31 @@ resource "google_vertex_ai_index" "non_compliant_example_1" {
   display_name    = "non-compliant-index-1"
   region          = "us-central1"
   deletion_policy = "DELETE"
+
+  metadata {
+    config {
+      dimensions = 2
+    }
+  }
 }
 
 resource "google_vertex_ai_index" "non_compliant_example_2" {
   display_name    = "non-compliant-index-2"
   region          = "us-central1"
   deletion_policy = "ABANDON"
+
+  metadata {
+    config {
+      dimensions = 2
+    }
+  }
 }
 
 resource "google_vertex_ai_index" "non_compliant_example_3" {
   display_name = "non-compliant-index-3"
   region       = "us-central1"
   # deletion_policy is omitted (defaults to DELETE)
-  
+
   metadata {
     config {
       dimensions = 2
