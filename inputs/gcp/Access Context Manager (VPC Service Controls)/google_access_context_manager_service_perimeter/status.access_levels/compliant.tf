dec30857744856dbd0fd48362a1c3a223993b849
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-31 21:13:55 +1000

    Update compliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/status.access_levels/compliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/status.access_levels/compliant.tf
@@ -1,7 +1,7 @@
 resource "google_access_context_manager_service_perimeter" "compliant_example_1" {
   parent = "accessPolicies/123456789"
   name   = "accessPolicies/123456789/servicePerimeters/compliant_status_access_levels"
-  title  = "compliant_status_access_levels"
+  title  = "service_perimeter"
 
   status {
     access_levels = [
