1e045a8b5a5a0fb4d59fdb2ee8743e39b6b4325b
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-31 21:57:06 +1000

    Update nonCompliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.egress_policies.egress_from.identities/nonCompliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.egress_policies.egress_from.identities/nonCompliant.tf
@@ -1,6 +1,6 @@
 resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
   parent = "accessPolicies/123456789"
-  name   = "compliant_egress_identities"
+  name   = "non_compliant_egress_identities"
   title  = "service_perimeter"
 
   spec {
