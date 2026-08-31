9b6860b651780068de82225a676659a414b223e0
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-09-01 06:12:56 +1000

    Classify dry run configuration as non-security

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/use_explicit_dry_run_spec/compliant.tf
+++
@@ -1,7 +0,0 @@
-resource "google_access_context_manager_service_perimeter" "compliant_example_1" {
-  parent = "accessPolicies/123456789"
-  name   = "compliant_explicit_dry_run"
-  title  = "service_perimeter"
-
-  use_explicit_dry_run_spec = false
-}
