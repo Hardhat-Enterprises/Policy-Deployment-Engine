c685cffed6c3f218722bda84a70decfe002d058b
Garry Zhuang <garryzhuang1@gmail.com>
2026-09-01 01:09:08 +1000

    fix: deletion_policy

--- inputs/gcp/Compute Engine/google_compute_resource_policy/deletion_policy/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_resource_policy/deletion_policy/compliant.tf
@@ -4,8 +4,7 @@ resource "google_compute_resource_policy" "compliant_example_1" {
   deletion_policy = "PREVENT"
 }
 resource "google_compute_resource_policy" "compliant_example_2" {
-  name         = "compliant_example_2"
-  zone         = "us-central1-a"
-  source_disk  = "projects/fake-project/zones/us-central1-a/disks/fake-disk"
+  name   = "compliant-example-2"
+  region = "us-central1"
   deletion_policy = "ABANDON"
 }
