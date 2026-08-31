4d58fe62ce64dc51a31dde1fc76eff479383405d
Navindra-cell <senonperera07@gmail.com>
2026-08-31 18:21:44 +1000

    Temporarily align endpoint fixture with dev

--- inputs/gcp/BigQuery/google_bigquery_routine/remote_function_options.endpoint/nonCompliant.tf
+++ inputs/gcp/BigQuery/google_bigquery_routine/remote_function_options.endpoint/nonCompliant.tf
@@ -5,14 +5,14 @@ resource "google_bigquery_routine" "non_compliant_example_1" {
   routine_type    = "SCALAR_FUNCTION"
   definition_body = "x * 2"
   language        = "SQL"
+  data_governance_type = "DATA_MASKING"
 
   remote_function_options {
-    endpoint          = "http://us-east1-my_gcf_project.cloudfunctions.net/remote_add"
-    connection        = "google_bigquery_connection.test.name"
+    endpoint = "https://us-east1-my_gcf_project.cloudfunctions.net/remote_add"
+    connection = "google_bigquery_connection.test.name"
     max_batching_rows = "10"
-
     user_defined_context = {
-      "z" = "1.5"
+      "z": "1.5",
     }
   }
 }
