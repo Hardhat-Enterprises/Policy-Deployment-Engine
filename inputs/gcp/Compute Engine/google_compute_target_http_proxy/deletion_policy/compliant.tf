bab8422ad02d6c5f940f29efa6c88f013b9ec833
Ruwindakarunasinghe166 <s225041211@deakin.edu.au>
2026-08-31 23:51:07 +1000

    Rename target HTTP proxy to compliant_example_1

--- inputs/gcp/Compute Engine/google_compute_target_http_proxy/deletion_policy/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_target_http_proxy/deletion_policy/compliant.tf
@@ -3,7 +3,7 @@
 # Only the tested resource type may appear in this file.
 
 resource "google_compute_target_http_proxy" "compliant_example_1" {
-  name            = "compliant-http-proxy"
+  name            = "compliant_example_1"
   url_map         = "https://www.googleapis.com/compute/v1/projects/example-project/global/urlMaps/example-url-map"
   deletion_policy = "PREVENT"
 }
