b71a704635250fb494915a764d2a4e7dc6645c96
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-31 21:13:04 +1000

    Update nonCompliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.vpc_accessible_services.enable_restriction/nonCompliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/spec.vpc_accessible_services.enable_restriction/nonCompliant.tf
@@ -1,7 +1,7 @@
 resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
   parent = "accessPolicies/123456789"
-  name   = "accessPolicies/123456789/servicePerimeters/noncompliant_enable_restriction"
-  title  = "noncompliant_enable_restriction"
+  name   = "accessPolicies/123456789/servicePerimeters/compliant_enable_restriction"
+  title  = "service_perimeter"
 
   spec {
     restricted_services = ["storage.googleapis.com"]
