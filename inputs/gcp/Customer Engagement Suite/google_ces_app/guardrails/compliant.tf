915fd85218bbdd5df43a1d5fb81d8b43515863f6
Adrian Bisangwa <adrian079william@gmail.com>
2026-08-31 22:54:20 +1000

    update using AI feedback

--- inputs/gcp/Customer Engagement Suite/google_ces_app/guardrails/compliant.tf
+++ inputs/gcp/Customer Engagement Suite/google_ces_app/guardrails/compliant.tf
@@ -1,10 +1,9 @@
 resource "google_ces_app" "compliant_example_1" {
-  app_id      = "app_id"
-  location    = "us"
+  app_id       = "app_id"
+  location     = "us"
   display_name = "my-app"
 
   guardrails = [
-    "projects/example-project/locations/us/apps/compliant-example-1/guardrails/example-guardrail"
+    "projects/*/locations/*/apps/*/guardrails/*"
   ]
 }
-
