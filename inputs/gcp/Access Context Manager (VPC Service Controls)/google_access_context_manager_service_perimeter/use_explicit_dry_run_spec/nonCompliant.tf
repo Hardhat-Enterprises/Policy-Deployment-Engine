9988751c9b066ac0459c85abe9414513eacd632d
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-31 20:47:50 +1000

    Update nonCompliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/use_explicit_dry_run_spec/nonCompliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/use_explicit_dry_run_spec/nonCompliant.tf
@@ -1,7 +1,7 @@
 resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
   parent = "accessPolicies/123456789"
-  name   = "non_compliant_explicit_dry_run"
-  title  = "explicit_dry_run_disabled"
+  name   = "compliant_explicit_dry_run"
+  title  = "service_perimeter"
 
   use_explicit_dry_run_spec = false
 }
