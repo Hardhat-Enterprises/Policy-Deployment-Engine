a5fc4cd9c57b0f136aa52e4f417d350a1c6dafb1
Your Full Name <your_github_email@example.com>
2026-08-31 15:54:22 +1000

    Fix regional security policy rule lint issues

--- inputs/gcp/Compute Engine/google_compute_region_security_policy_rule/network_match.src_region_codes/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_region_security_policy_rule/network_match.src_region_codes/nonCompliant.tf
@@ -3,7 +3,7 @@
 resource "google_compute_region_security_policy_rule" "non_compliant_example_1" {
   region          = "australia-southeast1"
   security_policy = "example-regional-security-policy"
-  priority        = 1001
+  priority        = 1000
   action          = "deny(403)"
 
   network_match {
--- inputs/gcp/Compute Engine/google_compute_region_security_policy_rule/region/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_region_security_policy_rule/region/nonCompliant.tf
@@ -3,7 +3,7 @@
 resource "google_compute_region_security_policy_rule" "non_compliant_example_1" {
   region          = "us-central1"
   security_policy = "example-regional-security-policy"
-  priority        = 1001
+  priority        = 1000
   action          = "deny(403)"
 
   match {
