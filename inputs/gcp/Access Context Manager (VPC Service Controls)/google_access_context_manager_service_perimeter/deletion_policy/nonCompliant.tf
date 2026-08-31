8ad3ec6572cebcca43bbcfc0a43ae572d12b35e8
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-08-31 21:55:16 +1000

    Update nonCompliant.tf

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter/deletion_policy/nonCompliant.tf
@@ -1,7 +1,7 @@
 # deletion_policy allows deletion
 resource "google_access_context_manager_service_perimeter" "non_compliant_example_1" {
   parent = "accessPolicies/123456789"
-  name   = "compliant_deletion_policy"
+  name   = "non_compliant_deletion_policy"
   title  = "service_perimeter"
 
   deletion_policy = "DELETE"
