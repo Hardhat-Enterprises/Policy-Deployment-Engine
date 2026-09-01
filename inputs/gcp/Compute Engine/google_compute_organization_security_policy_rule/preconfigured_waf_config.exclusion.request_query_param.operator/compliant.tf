0ac684d82b2f64c1768185cb3ddfddb4cc5d17e7
Ashi-Athu <ashiathu10@gmail.com>
2026-09-01 17:31:22 +1000

    Create compliant.tf

---
+++ inputs/gcp/Compute Engine/google_compute_organization_security_policy_rule/preconfigured_waf_config.exclusion.request_query_param.operator/compliant.tf
@@ -0,0 +1,30 @@
+resource "google_compute_organization_security_policy" "policy" {
+  short_name = "tf-test-policy"
+  parent     = "organizations/123456789"
+  type       = "CLOUD_ARMOR"
+}
+
+resource "google_compute_organization_security_policy_rule" "compliant_example_1" {
+  policy_id   = google_compute_organization_security_policy.policy.id
+  action      = "allow"
+  description = "compliant_example_1"
+
+  match {
+    expr {
+      expression = "evaluatePreconfiguredWaf('sqli-stable', {'sensitivity': 2})"
+    }
+    versioned_expr = ""
+  }
+
+  preconfigured_waf_config {
+    exclusion {
+      target_rule_set = "sqli-stable"
+      request_query_param {
+        operator = "EQUALS"
+        value    = "user_input"
+      }
+    }
+  }
+
+  priority = 100
+}
