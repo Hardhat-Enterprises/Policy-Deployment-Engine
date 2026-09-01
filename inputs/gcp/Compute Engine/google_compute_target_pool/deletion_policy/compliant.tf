db7c7bc11bfdb14b891c0bb958a36e08ccc4d3ef
ashlycorreya <ashlycorreya444@gmail.com>
2026-09-01 11:49:48 +1000

    Fix target pool deletion policy assessment

--- inputs/gcp/Compute Engine/google_compute_target_pool/deletion_policy/compliant.tf
+++
@@ -1,4 +0,0 @@
-resource "google_compute_target_pool" "compliant_example_1" {
-  name            = "compliant-example-1"
-  deletion_policy = "DELETE"
-}
--- inputs/gcp/Compute Engine/google_compute_target_pool/deletion_policy/nonCompliant.tf
+++
@@ -1,4 +0,0 @@
-resource "google_compute_target_pool" "non_compliant_example_1" {
-  name            = "non-compliant-example-1"
-  deletion_policy = "ABANDON"
-}
