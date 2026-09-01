4857385672b3d7a4ee73da0e49e26c8790fcc71f
Ashi-Athu <ashiathu10@gmail.com>
2026-09-01 14:47:35 +1000

    Create compliant.tf

---
+++ inputs/gcp/Compute Engine/google_compute_organization_security_policy_rule/deletion_policy/compliant.tf
@@ -0,0 +1,21 @@
+resource "google_compute_organization_security_policy" "policy" {
+  short_name = "tf-test-policy"
+  parent     = "organizations/123456789"
+  type       = "CLOUD_ARMOR"
+}
+
+resource "google_compute_organization_security_policy_rule" "compliant_example_1" {
+  policy_id       = google_compute_organization_security_policy.policy.id
+  action          = "allow"
+  description     = "compliant_example_1"
+  deletion_policy = "PREVENT"
+
+  match {
+    config {
+      src_ip_ranges = ["192.168.0.0/16"]
+    }
+    versioned_expr = "SRC_IPS_V1"
+  }
+
+  priority = 100
+}
