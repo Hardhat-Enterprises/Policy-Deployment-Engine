b54d67dde2c91195d9ea125d28d89a969ab53858
ashlycorreya <ashlycorreya444@gmail.com>
2026-09-01 12:37:37 +1000

    Mark IPv6 access type as an architectural choice

--- inputs/gcp/Compute Engine/google_compute_public_advertised_prefix/ipv6_access_type/nonCompliant.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_public_advertised_prefix" "non_compliant_example_1" {
-  name             = "non-compliant-example-1"
-  ip_cidr_range    = "2001:db8::/32"
-  pdp_scope        = "REGIONAL"
-  ipv6_access_type = "EXTERNAL"
-}
