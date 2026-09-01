18be8e7c14b7bbcb1e485dbabf38370cc170cf10
ashlycorreya <ashlycorreya444@gmail.com>
2026-09-01 20:49:23 +1000

    Fix region network endpoint fixture identities

--- inputs/gcp/Compute Engine/google_compute_region_network_endpoint/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_region_network_endpoint/deletion_policy/nonCompliant.tf
@@ -1,5 +1,5 @@
 resource "google_compute_region_network_endpoint" "non_compliant_example_1" {
-  region_network_endpoint_group = "non-compliant-example-1"
+  region_network_endpoint_group = "non_compliant_example_1"
   region                        = "australia-southeast1"
   ip_address                    = "8.8.8.8"
   port                          = 443
