ebf70e3e197d1eeea38d810c0621c266cdb4af2a
Ashi-Athu <ashiathu10@gmail.com>
2026-09-01 20:45:06 +1000

    Update nonCompliant.tf

--- inputs/gcp/Compute Engine/google_compute_organization_security_policy_rule/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_organization_security_policy_rule/deletion_policy/nonCompliant.tf
@@ -1,11 +1,5 @@
-resource "google_compute_organization_security_policy" "policy" {
-  short_name = "tf-test-policy"
-  parent     = "organizations/123456789"
-  type       = "CLOUD_ARMOR"
-}
-
 resource "google_compute_organization_security_policy_rule" "non_compliant_example_1" {
-  policy_id       = google_compute_organization_security_policy.policy.id
+  policy_id       = "organizations/123456789/securityPolicies/1234567890"
   action          = "allow"
   description     = "non_compliant_example_1"
   deletion_policy = "DELETE"
