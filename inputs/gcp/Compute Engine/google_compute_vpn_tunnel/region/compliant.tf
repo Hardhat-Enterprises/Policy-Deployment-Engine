6d502ff0b5ee0859f874a45e59cb58f422a8c377
Karim <Sulimankemo43@gmail.com>
2026-09-02 00:03:51 +1000

    Fix fixture drift in VPN tunnel policies and refresh plan cache

--- inputs/gcp/Compute Engine/google_compute_vpn_tunnel/region/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_vpn_tunnel/region/compliant.tf
@@ -2,6 +2,6 @@ resource "google_compute_vpn_tunnel" "compliant_example_1" {
   name          = "tunnel-compliant-1"
   peer_ip       = "15.0.0.120"
   shared_secret = "a secret message"
-  target_vpn_gateway = "projects/fake-project/regions/australia-southeast1/targetVpnGateways/fake-gateway"
+  target_vpn_gateway = "fake-gateway"
   region = "australia-southeast1"
 }
--- inputs/gcp/Compute Engine/google_compute_vpn_tunnel/region/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_vpn_tunnel/region/nonCompliant.tf
@@ -1,7 +1,7 @@
 resource "google_compute_vpn_tunnel" "non_compliant_example_1" {
   name          = "tunnel-noncompliant-1"
-  peer_ip       = "15.0.0.121"
+  peer_ip       = "15.0.0.120"
   shared_secret = "a secret message"
-  target_vpn_gateway = "projects/fake-project/regions/us-central1/targetVpnGateways/fake-gateway"
+  target_vpn_gateway = "fake-gateway"
   region = "us-central1"
 }
