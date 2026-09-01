4ffb15b9a975b859a5073a8d642aa12fd4ed62ce
Rithwik9889 <pokarithwik@gmail.com>
2026-09-01 17:04:21 +1000

    Revert create_snapshot_before_destroy to false, remove policy/fixtures, update rationales

--- inputs/gcp/Compute Engine/google_compute_disk/create_snapshot_before_destroy/compliant.tf
+++
@@ -1,5 +0,0 @@
-resource "google_compute_disk" "compliant_example_1" {
-  name                           = "compliant-example-1"
-  zone                           = "us-central1-a"
-  create_snapshot_before_destroy  = true
-}
--- inputs/gcp/Compute Engine/google_compute_disk/create_snapshot_before_destroy/nonCompliant.tf
+++
@@ -1,5 +0,0 @@
-resource "google_compute_disk" "non_compliant_example_1" {
-  name                           = "non-compliant-example-1"
-  zone                           = "us-central1-a"
-  create_snapshot_before_destroy  = false
-}
