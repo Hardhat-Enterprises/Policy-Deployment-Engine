eb952189d25407e33a64089ab0e4239cd6e67dd4
shey9855 <shehaniwije1998@gmail.com>
2026-09-01 22:39:17 +1000

    Fix oODB subnet policy fixtures

--- inputs/gcp/Oracle Database/google_oracle_database_odb_subnet/deletion_protection/nonCompliant.tf
+++ inputs/gcp/Oracle Database/google_oracle_database_odb_subnet/deletion_protection/nonCompliant.tf
@@ -1,5 +1,5 @@
 resource "google_oracle_database_odb_subnet" "non_compliant_example_1" {
-  odb_subnet_id = "non-compliant-example-1"
+  odb_subnet_id = "test-subnet-1"
   location      = "australia-southeast1"
   project       = "my-project"
   odbnetwork    = "my-odbnetwork"
