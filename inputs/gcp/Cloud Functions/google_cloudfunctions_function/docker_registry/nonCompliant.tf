2edff7c8821d72706d94c5ae896ef1cfc4e9b291
jamiduo <jaydend1000@live.com.au>
2026-09-01 15:04:08 +1000

    Pulled Origin/dev and updated documentation

--- inputs/gcp/Cloud Functions/google_cloudfunctions_function/docker_registry/nonCompliant.tf
+++ inputs/gcp/Cloud Functions/google_cloudfunctions_function/docker_registry/nonCompliant.tf
@@ -1,6 +1,6 @@
 resource "google_cloudfunctions_function" "non_compliant_example_1" {
   name            = "non_compliant_example_1"
-  runtime         = "Node.js10"
+  runtime         = "Python3.12"
   region          = "google_cloudfunctions_function.function.region"
   project         = "google_cloudfunctions_function.function.project"
   docker_registry = "CONTAINER_REGISTRY"
