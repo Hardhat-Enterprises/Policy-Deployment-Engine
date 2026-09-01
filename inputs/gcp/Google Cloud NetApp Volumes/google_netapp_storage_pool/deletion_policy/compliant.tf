2cc3dbccf4c4099008e5db96d7bbd834d2d85587
archana <archanamucha.0501@gmail.com>
2026-09-01 21:55:03 +1000

    Fix deletion policy fixture drift

--- inputs/gcp/Google Cloud NetApp Volumes/google_netapp_storage_pool/deletion_policy/compliant.tf
+++ inputs/gcp/Google Cloud NetApp Volumes/google_netapp_storage_pool/deletion_policy/compliant.tf
@@ -1,9 +1,9 @@
 resource "google_netapp_storage_pool" "compliant_example_1" {
   project       = "deakin-lab-123"
   name          = "compliant_example_1"
-  location      = "australia-southeast2"                 # AU (OK)
+  location      = "australia-southeast2"                 
   service_level = "PREMIUM"
   capacity_gib  = 2048
-  network       = "projects/deakin-lab-123/global/networks/nondefault-vpc"  # non-default VPC
+  network       = "projects/deakin-lab-123/global/networks/nondefault-vpc"  
   deletion_policy = "PREVENT"
 }
--- inputs/gcp/Google Cloud NetApp Volumes/google_netapp_storage_pool/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Google Cloud NetApp Volumes/google_netapp_storage_pool/deletion_policy/nonCompliant.tf
@@ -1,9 +1,9 @@
 resource "google_netapp_storage_pool" "non_compliant_example_1" {
   project       = "deakin-lab-123"
   name          = "non_compliant_example_1"
-  location      = "us-central1"                          # non-AU (NC)
+  location      = "australia-southeast2"                          
   service_level = "PREMIUM"
   capacity_gib  = 2048
-  network       = "projects/deakin-lab-123/global/networks/nondefault-vpc"         # default VPC (NC)
+  network       = "projects/deakin-lab-123/global/networks/nondefault-vpc"         
   deletion_policy = "DELETE"
 }
