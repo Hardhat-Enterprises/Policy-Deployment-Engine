ae1c12672475ea9bcb335387c49bf12e8c563571
s225511702 <s225511702@deakin.edu.au>
2026-09-01 21:03:10 +1000

    fix: correct client_certificate plugin_instance to false per reviewer feedback and remove policy; rewrite server_validation_ca rationale

--- inputs/gcp/Network Security/google_network_security_client_tls_policy/client_certificate.certificate_provider_instance.plugin_instance/compliant.tf
+++
@@ -1,9 +0,0 @@
-resource "google_network_security_client_tls_policy" "compliant_example_1" {
-  name = "compliant_example_1"
-
-  client_certificate {
-    certificate_provider_instance {
-      plugin_instance = "google_cloud_private_spiffe"
-    }
-  }
-}
--- inputs/gcp/Network Security/google_network_security_client_tls_policy/client_certificate.certificate_provider_instance.plugin_instance/nonCompliant.tf
+++
@@ -1,9 +0,0 @@
-resource "google_network_security_client_tls_policy" "non_compliant_example_1" {
-  name = "non_compliant_example_1"
-
-  client_certificate {
-    certificate_provider_instance {
-      plugin_instance = "custom_unmanaged_plugin"
-    }
-  }
-}
