7a547cb95f3e9b28d27b409d8cc53ce359a2cf2f
s225511702 <s225511702@deakin.edu.au>
2026-09-02 16:37:22 +1000

    fix: revert custom_tls_features to false and remove policy per reviewer feedback - arbitrary feature list cannot be exhaustively validated generically

--- inputs/gcp/Network Security/google_network_security_tls_inspection_policy/custom_tls_features/nonCompliant.tf
+++
@@ -1,6 +0,0 @@
-resource "google_network_security_tls_inspection_policy" "non_compliant_example_1" {
-  name                  = "non_compliant_example_1"
-  ca_pool                = "projects/example-project/locations/global/caPools/example-pool"
-  tls_feature_profile   = "PROFILE_CUSTOM"
-  custom_tls_features   = ["TLS_RSA_WITH_RC4_128_SHA"]
-}
