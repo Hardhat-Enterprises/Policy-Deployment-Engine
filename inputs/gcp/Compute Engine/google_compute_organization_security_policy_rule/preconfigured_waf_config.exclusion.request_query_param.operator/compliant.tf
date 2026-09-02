92e34914d808a8cf0c9107086b64e4222186c76c
Ashi-Athu <ashiathu10@gmail.com>
2026-09-02 12:01:52 +1000

    Update compliant.tf

--- inputs/gcp/Compute Engine/google_compute_organization_security_policy_rule/preconfigured_waf_config.exclusion.request_query_param.operator/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_organization_security_policy_rule/preconfigured_waf_config.exclusion.request_query_param.operator/compliant.tf
@@ -1,11 +1,5 @@
-resource "google_compute_organization_security_policy" "policy" {
-  short_name = "tf-test-policy"
-  parent     = "organizations/123456789"
-  type       = "CLOUD_ARMOR"
-}
-
 resource "google_compute_organization_security_policy_rule" "compliant_example_1" {
-  policy_id   = google_compute_organization_security_policy.policy.id
+  policy_id   = "organizations/123456789/securityPolicies/1234567890"
   action      = "allow"
   description = "compliant_example_1"
 
