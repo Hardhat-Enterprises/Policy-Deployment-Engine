bad7fb83369be1f9eed7c6b8696283384ab3a3c9
narasimharaomedasani9909-cmd <narasimharaomedasani9909@gmail.com>
2026-09-02 13:17:54 +1000

    Remove access policy files from service perimeter branch

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_access_policy/deletion_policy/nonCompliant.tf
+++
@@ -1,6 +0,0 @@
-resource "google_access_context_manager_access_policy" "non_compliant_example_1" {
-  parent = "organizations/123456789"
-  title  = "non-compliant-access-policy"
-
-  deletion_policy = "DELETE"
-}
