00e7e8b50cb148a3a35f78a8873f00d3ca85ed31
s225511702 <s225511702@deakin.edu.au>
2026-09-02 16:12:49 +1000

    fix: correct server_validation_ca plugin_instance to false per reviewer feedback and remove policy for consistency with client-side field

--- inputs/gcp/Network Security/google_network_security_client_tls_policy/server_validation_ca.certificate_provider_instance.plugin_instance/compliant.tf
+++
@@ -1,9 +0,0 @@
-resource "google_network_security_client_tls_policy" "compliant_example_1" {
-  name = "compliant_example_1"
-
-  server_validation_ca {
-    certificate_provider_instance {
-      plugin_instance = "google_cloud_private_spiffe"
-    }
-  }
-}
--- inputs/gcp/Network Security/google_network_security_client_tls_policy/server_validation_ca.certificate_provider_instance.plugin_instance/nonCompliant.tf
+++
@@ -1,9 +0,0 @@
-resource "google_network_security_client_tls_policy" "non_compliant_example_1" {
-  name = "non_compliant_example_1"
-
-  server_validation_ca {
-    certificate_provider_instance {
-      plugin_instance = "custom_unmanaged_plugin"
-    }
-  }
-}
