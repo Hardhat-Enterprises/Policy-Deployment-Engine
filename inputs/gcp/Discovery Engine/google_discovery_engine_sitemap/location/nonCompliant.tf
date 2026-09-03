84c2224f5fd229b1cae424e756855b41e8d6778b
Sakka-arch <anirudhgupta.2604@gmail.com>
2026-09-03 23:19:17 +1000

    Add discovery engine sitemap policy

--- inputs/gcp/Discovery Engine/google_discovery_engine_sitemap/location/nonCompliant.tf
+++ inputs/gcp/Discovery Engine/google_discovery_engine_sitemap/location/nonCompliant.tf
@@ -2,6 +2,6 @@ resource "google_discovery_engine_sitemap" "non_compliant_example_1" {
     data_store_id = "non_compliant_example_1"
     location      = "us"
     uri           = "https://www.google.com/sitemap.xml"
-    project       = "Capstone project"
+    project       = "capstone project"
 
 }
