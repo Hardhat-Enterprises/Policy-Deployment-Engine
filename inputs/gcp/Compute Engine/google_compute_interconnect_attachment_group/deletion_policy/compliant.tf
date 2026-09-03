d36f2f821c70c632e8a0eda319333b094fed0fad
connor-deakin <cdruiven@deakin.edu.au>
2026-09-03 19:21:33 +1000

    gcp/Compute Engine/google_compute_interconnect_attachment_group Fix: policy name formatting

--- inputs/gcp/Compute Engine/google_compute_interconnect_attachment_group/deletion_policy/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_interconnect_attachment_group/deletion_policy/compliant.tf
@@ -1,7 +1,10 @@
 resource "google_compute_interconnect_attachment_group" "compliant_example_1" {
-  name = "compliant_example_1"
+  name = "compliant-example-1"
+  project = "my-project"
+  deletion_policy = "PREVENT"
+  
   intent {
     availability_sla = "NO_SLA"
   }
-  deletion_policy = "PREVENT"
+  
 }
--- inputs/gcp/Compute Engine/google_compute_interconnect_attachment_group/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_interconnect_attachment_group/deletion_policy/nonCompliant.tf
@@ -1,7 +1,10 @@
 resource "google_compute_interconnect_attachment_group" "non_compliant_example_1" {
-  name = "non_compliant_example_1"
+  name = "non-compliant-example-1"
+  project = "my-project"
+  deletion_policy = "DELETE"
+
   intent {
     availability_sla = "NO_SLA"
   }
-  deletion_policy = "DELETE"
+  
 }
