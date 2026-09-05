3436bc87f7a46d2d0edeb9a3a89f64e29cff4e17
archana <archanamucha.0501@gmail.com>
2026-09-05 22:34:37 +1000

    Fix kms_config structural validation

--- inputs/gcp/Google Cloud NetApp Volumes/google_netapp_storage_pool/kms_config/compliant.tf
+++ inputs/gcp/Google Cloud NetApp Volumes/google_netapp_storage_pool/kms_config/compliant.tf
@@ -1,10 +1,10 @@
 resource "google_netapp_storage_pool" "compliant_example_1" {
-  project       = "deakin-lab-123"
-  name          = "compliant-example-1"
+  project       = "example-project"
+  name          = "compliant_example_1"
   location      = "australia-southeast2"
   service_level = "PREMIUM"
   capacity_gib  = 2048
-  network       = "projects/deakin-lab-123/global/networks/nondefault-vpc"
+  network       = "projects/example-project/global/networks/example-vpc"
 
-  kms_config = "projects/deakin-lab-123/locations/australia-southeast2/kmsConfigs/example-kms-config"
+  kms_config = "projects/example-project/locations/australia-southeast2/kmsConfigs/example-config"
 }
--- inputs/gcp/Google Cloud NetApp Volumes/google_netapp_storage_pool/kms_config/nonCompliant.tf
+++ inputs/gcp/Google Cloud NetApp Volumes/google_netapp_storage_pool/kms_config/nonCompliant.tf
@@ -1,10 +1,10 @@
 resource "google_netapp_storage_pool" "non_compliant_example_1" {
-  project       = "deakin-lab-123"
+  project       = "example-project"
   name          = "non_compliant_example_1"
   location      = "australia-southeast2"
   service_level = "PREMIUM"
   capacity_gib  = 2048
-  network       = "projects/deakin-lab-123/global/networks/nondefault-vpc"
+  network       = "projects/example-project/global/networks/example-vpc"
 
-  # kms_config is intentionally omitted
+  kms_config = "invalid-kms-config"
 }
