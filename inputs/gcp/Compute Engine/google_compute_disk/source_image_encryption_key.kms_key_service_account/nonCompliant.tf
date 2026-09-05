7860491a197757660e88d7ed0a8d189644c21736
Rithwik9889 <pokarithwik@gmail.com>
2026-09-05 23:52:06 +1000

    Fix kms_key_service_account fixtures, add all plan caches for google_compute_disk

--- inputs/gcp/Compute Engine/google_compute_disk/source_image_encryption_key.kms_key_service_account/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_disk/source_image_encryption_key.kms_key_service_account/nonCompliant.tf
@@ -2,7 +2,4 @@ resource "google_compute_disk" "non_compliant_example_1" {
   name  = "non-compliant-example-1"
   zone  = "australia-southeast1-a"
   image = "projects/debian-cloud/global/images/debian-11-bullseye-v20240110"
-
-  source_image_encryption_key {
-  }
 }
--- inputs/gcp/Compute Engine/google_compute_disk/source_snapshot_encryption_key.kms_key_service_account/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_disk/source_snapshot_encryption_key.kms_key_service_account/nonCompliant.tf
@@ -2,7 +2,4 @@ resource "google_compute_disk" "non_compliant_example_1" {
   name     = "non-compliant-example-1"
   zone     = "australia-southeast1-a"
   snapshot = "projects/my-project/global/snapshots/my-snapshot"
-
-  source_snapshot_encryption_key {
-  }
 }
