9a8b74b58cd2b08be3551b7ac533c2c5f105bf7a
Rithwik9889 <pokarithwik@gmail.com>
2026-09-05 16:40:11 +1000

    Fix deletion_policy fixture-drift: match interface blocks

--- inputs/gcp/Compute Engine/google_compute_external_vpn_gateway/deletion_policy/compliant.tf
+++ inputs/gcp/Compute Engine/google_compute_external_vpn_gateway/deletion_policy/compliant.tf
@@ -1,6 +1,5 @@
 resource "google_compute_external_vpn_gateway" "compliant_example_1" {
   name            = "compliant-example-1"
-  redundancy_type = "SINGLE_IP_INTERNALLY_REDUNDANT"
   deletion_policy = "PREVENT"
 
   interface {
--- inputs/gcp/Compute Engine/google_compute_external_vpn_gateway/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_external_vpn_gateway/deletion_policy/nonCompliant.tf
@@ -1,10 +1,9 @@
 resource "google_compute_external_vpn_gateway" "non_compliant_example_1" {
   name            = "non-compliant-example-1"
-  redundancy_type = "SINGLE_IP_INTERNALLY_REDUNDANT"
   deletion_policy = "DELETE"
 
   interface {
     id         = 0
-    ip_address = "203.0.113.2"
+    ip_address = "203.0.113.1"
   }
 }
