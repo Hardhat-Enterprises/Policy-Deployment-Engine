d8350e2f54f862e2fdb6f1afdad09713d722df91
Sakka-arch <anirudhgupta.2604@gmail.com>
2026-09-06 23:38:31 +1000

    Fix rationales and remove non-security policies for google_discovery_engine_widget_config

--- inputs/gcp/Discovery Engine/google_discovery_engine_widget_config/ui_settings.disable_user_events_collection/compliant.tf
+++
@@ -1,8 +0,0 @@
-resource "google_discovery_engine_widget_config" "compliant_example_1" {
-    engine_id  = "compliant_example_1"
-    location   = "eu"
-    project    = "capstone project"
-    ui_settings {
-        disable_user_events_collection = true
-    }
-}
