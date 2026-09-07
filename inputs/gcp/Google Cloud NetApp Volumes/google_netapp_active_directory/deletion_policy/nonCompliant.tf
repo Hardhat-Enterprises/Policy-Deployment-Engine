71a09f18978080a1896d66461896119f68e3bd0c
archana <archanamucha.0501@gmail.com>
2026-09-07 20:47:57 +1000

    Fix active directory deletion policy fixture drift

--- inputs/gcp/Google Cloud NetApp Volumes/google_netapp_active_directory/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Google Cloud NetApp Volumes/google_netapp_active_directory/deletion_policy/nonCompliant.tf
@@ -1,7 +1,7 @@
 resource "google_netapp_active_directory" "non_compliant_example_1" {
   project         = "deakin-lab-123"
   name            = "non_compliant_example_1"
-  location        = "us-central1"
+  location        = "australia-southeast2"
   domain          = "deakin.internal"
   dns             = "10.10.0.10"
   net_bios_prefix = "smbserver"
