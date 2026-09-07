ee66b50b95b90cbb87878bddd1cbc5b2039b2cba
Motheo Boikhutso <mboikhutso1@gmail.com>
2026-09-07 18:33:50 +1000

    Fix linter errors for stored info type

--- inputs/gcp/Data Loss Prevention/google_data_loss_prevention_stored_info_type/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Data Loss Prevention/google_data_loss_prevention_stored_info_type/deletion_policy/nonCompliant.tf
@@ -1,7 +1,8 @@
 resource "google_data_loss_prevention_stored_info_type" "non_compliant_example_1" {
-  parent       = "projects/my-project-name/locations/us-central1"
-  description  = "Non-compliant example"
-  display_name = "non_compliant_example_1"
+  parent          = "projects/my-project-name"
+  description     = "Example"
+  display_name    = "non_compliant_example_1"
+  deletion_policy = "DELETE"
 
   regex {
     pattern = "patient"
