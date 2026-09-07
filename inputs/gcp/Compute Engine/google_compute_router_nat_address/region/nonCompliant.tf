e84832bd1db2b39acef17113abdbb2acd7f35a05
Ruwindakarunasinghe166 <s225041211@deakin.edu.au>
2026-09-08 01:55:29 +1000

    Change router_nat name in nonCompliant.tf

--- inputs/gcp/Compute Engine/google_compute_router_nat_address/region/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_router_nat_address/region/nonCompliant.tf
@@ -1,6 +1,6 @@
 resource "google_compute_router_nat_address" "non_compliant_example_1" {
   router          = "example-router"
-  router_nat      = "example-router-nat"
+  router_nat      = "non-compliant-example-1"
   region          = "us-central1"
   nat_ips         = ["https://www.googleapis.com/compute/v1/projects/example-project/regions/australia-southeast1/addresses/example-nat-ip"]
   deletion_policy = "PREVENT"
