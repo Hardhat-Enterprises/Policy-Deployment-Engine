a0ff6f0730fef423d91bcdde594bf70f98c7efd2
Ruwindakarunasinghe166 <s225041211@deakin.edu.au>
2026-09-08 01:45:47 +1000

    Add non-compliant NAT address resource for GCP

---
+++ inputs/gcp/Compute Engine/google_compute_router_nat_address/region/nonCompliant.tf
@@ -0,0 +1,7 @@
+resource "google_compute_router_nat_address" "non_compliant_example_1" {
+  router          = "example-router"
+  router_nat      = "example-router-nat"
+  region          = "us-central1"
+  nat_ips         = ["https://www.googleapis.com/compute/v1/projects/example-project/regions/australia-southeast1/addresses/example-nat-ip"]
+  deletion_policy = "PREVENT"
+}
