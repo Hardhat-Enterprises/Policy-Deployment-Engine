90bd06451ccf11167dde67b3936bdf8769214a26
Ruwindakarunasinghe166 <s225041211@deakin.edu.au>
2026-09-08 06:45:35 +1000

    Change deletion policy from DELETE to ABANDON

--- inputs/gcp/Compute Engine/google_compute_target_grpc_proxy/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_target_grpc_proxy/deletion_policy/nonCompliant.tf
@@ -1,4 +1,4 @@
 resource "google_compute_target_grpc_proxy" "non_compliant_example_1" {
   name            = "non-compliant-example-1"
-  deletion_policy = "DELETE"
+  deletion_policy = "ABANDON"
 }
