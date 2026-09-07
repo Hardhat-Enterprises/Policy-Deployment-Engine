d6e63a2035621cc053378c2e09e277189f911b30
Ruwindakarunasinghe166 <s225041211@deakin.edu.au>
2026-09-08 02:07:02 +1000

    Change deletion policy from DELETE to ABANDON

--- inputs/gcp/Compute Engine/google_compute_router_nat_address/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Compute Engine/google_compute_router_nat_address/deletion_policy/nonCompliant.tf
@@ -3,5 +3,5 @@ resource "google_compute_router_nat_address" "non_compliant_example_1" {
   router_nat      = "non-compliant-example-1"
   region          = "australia-southeast1"
   nat_ips         = ["https://www.googleapis.com/compute/v1/projects/example-project/regions/australia-southeast1/addresses/example-nat-ip"]
-  deletion_policy = "DELETE"
+  deletion_policy = "ABANDON"
 }
