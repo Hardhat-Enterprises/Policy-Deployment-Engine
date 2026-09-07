15316c73b449aa3a71dddeba519293219fbee6d9
Ruwindakarunasinghe166 <s225041211@deakin.edu.au>
2026-09-07 20:48:14 +1000

    Reclassify association deletion policy as non-security impacting

--- inputs/gcp/Compute Engine/google_compute_organization_security_policy_association/deletion_policy/nonCompliant.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_organization_security_policy_association" "non_compliant_example_1" {
-  name            = "non-compliant-example-1"
-  attachment_id   = "organizations/123456789"
-  policy_id       = "locations/global/securityPolicies/example-policy"
-  deletion_policy = "DELETE"
-}
