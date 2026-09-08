752bee53971c3637bd3c26941d40edd4e37e14de
kushagra saxena <kush.saxena123@outlook.com>
2026-09-08 07:39:55 +0000

    Add DLP job trigger parent location policy

--- inputs/gcp/Data Loss Prevention/google_data_loss_prevention_job_trigger/parent/nonCompliant.tf
+++ inputs/gcp/Data Loss Prevention/google_data_loss_prevention_job_trigger/parent/nonCompliant.tf
@@ -1,6 +1,6 @@
 resource "google_data_loss_prevention_job_trigger" "non_compliant_example_1" {
-  parent = "projects/example-project/locations/australia-southeast1"
-  status = "PAUSED"
+  parent = "projects/example-project/locations/us-central1"
+  status = "HEALTHY"
 
   triggers {
     schedule {
