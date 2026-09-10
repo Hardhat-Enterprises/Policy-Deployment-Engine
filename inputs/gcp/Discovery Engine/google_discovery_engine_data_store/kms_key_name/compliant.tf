db9a3b6c511c5530bb13319ea4f196910d234e12
Sakka-arch <anirudhgupta.2604@gmail.com>
2026-09-10 22:21:02 +1000

    removed extra resource

--- inputs/gcp/Discovery Engine/google_discovery_engine_data_store/kms_key_name/compliant.tf
+++ inputs/gcp/Discovery Engine/google_discovery_engine_data_store/kms_key_name/compliant.tf
@@ -1,11 +1,14 @@
+# data_store_kms_key_name
+
 resource "google_discovery_engine_data_store" "compliant_example_1" {
-    project               = "735927692082"
-    location              = "eu"
-    data_store_id         = "compliant_example_1"
-    display_name          = "compliant_example_1"
-    industry_vertical     = "GENERIC"
-    content_config        = "NO_CONTENT"
-    solution_types        = ["SOLUTION_TYPE_SEARCH"]
-    create_advanced_site_search = false
-    kms_key_name          = "projects/my-project/locations/eu/keyRings/my-ring/cryptoKeys/my-key"
+project = "735927692082"
+  location                    = "us"
+  data_store_id               = "compliant_example_1"
+  display_name                = "tf-test-structured-datastore"
+  industry_vertical           = "GENERIC"
+  content_config              = "NO_CONTENT"
+  solution_types              = ["SOLUTION_TYPE_SEARCH"]
+  kms_key_name                = "/project/keys/my-safe-key"
+  create_advanced_site_search = false
 }
+
