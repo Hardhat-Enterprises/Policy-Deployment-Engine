ee53c6905e7232914df404bdc2e3feb67ccbdf79
Rom Fridman <s222254964@deakin.edu.au>
2026-09-10 17:19:39 +1000

    Fix google_dataplex_entry fixtures

--- inputs/gcp/Dataplex/google_dataplex_entry/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Dataplex/google_dataplex_entry/deletion_policy/nonCompliant.tf
@@ -1,7 +1,7 @@
 resource "google_dataplex_entry" "non_compliant_example_1" {
   entry_group_id  = "@bigquery"
-  project         = "non_compliant_example_1"
-  location        = "us-central1"
+  project         = "compliant_example_1"
+  location        = "australia-southeast1"
   entry_id        = "entry-basic"
   entry_type      = "projects/655216118709/locations/global/entryTypes/bigquery-table"
 
