69e95ef36f5b200ed95b48e656974fd8323ed266
gauthamrmesh <gautham.music@gmail.com>
2026-09-11 20:10:22 +1000

    Add deletion_policy and location policies with fixtures

---
+++ inputs/gcp/Cloud Deploy/google_clouddeploy_delivery_pipeline/location/compliant.tf
@@ -0,0 +1,5 @@
+resource "google_clouddeploy_delivery_pipeline" "compliant_example_1" {
+  name      = "compliant_example_1"
+  location  = "australia-southeast1"
+  project   = "my-project-name"
+}
---
+++ inputs/gcp/Cloud Deploy/google_clouddeploy_delivery_pipeline/location/nonCompliant.tf
@@ -0,0 +1,5 @@
+resource "google_clouddeploy_delivery_pipeline" "non_compliant_example_1" {
+  name      = "non_compliant_example_1"
+  location  = "us-central1"
+  project   = "my-project-name"
+}
