514032735ed381bac2391e6491abe98a9bdab3ac
jamiduo <jaydend1000@live.com.au>
2026-09-11 17:59:03 +1000

    Restore shared files from dev

--- inputs/gcp/Discovery Engine/google_discovery_engine_control/location/compliant.tf
+++ inputs/gcp/Discovery Engine/google_discovery_engine_control/location/compliant.tf
@@ -5,11 +5,7 @@ resource "google_discovery_engine_control" "compliant_example_1" {
   location      = "us"
   engine_id     = "engine-id"
   control_id    = "compliant-example-1"
-<<<<<<< HEAD
-  display_name  = "Approved-location control"
-=======
   display_name  = "Example control"
->>>>>>> origin/dev
   solution_type = "SOLUTION_TYPE_SEARCH"
   use_cases     = ["SEARCH_USE_CASE_SEARCH"]
 
