38bcbd4b7adf39ec84c7a7b7dc94e123c767b755
gauthamrmesh <gautham.music@gmail.com>
2026-09-13 22:44:51 +1000

    Reclassify producer_accept_lists and subnetworks as security_impact:false per tutor-confirmed reviewer feedback - not platform-enforceable without hardcoding team-specific values

--- inputs/gcp/Compute Engine/google_compute_network_attachment/producer_accept_lists/nonCompliant.tf
+++
@@ -1,8 +0,0 @@
-resource "google_compute_network_attachment" "non_compliant_example_1" {
-  name                   = "noncompliant-attachment-1"
-  connection_preference  = "ACCEPT_MANUAL"
-  subnetworks = [
-    "projects/approved-project/regions/australia-southeast1/subnetworks/approved-subnetwork"
-  ]
-  producer_accept_lists = []
-}
--- inputs/gcp/Compute Engine/google_compute_network_attachment/subnetworks/compliant.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_network_attachment" "compliant_example_1" {
-  name                  = "compliant-attachment-1"
-  connection_preference = "ACCEPT_MANUAL"
-  subnetworks = [
-    "projects/project-1/regions/australia-southeast1/subnetworks/subnetwork-1"
-  ]
-}
