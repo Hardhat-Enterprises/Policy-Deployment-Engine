f0df7b12063622a09b69156232a69070c8102eef
kisara <s225209339@deakin.edu.au>
2026-09-13 21:42:24 +1000

    Add multicast group consumer activation policies

---
+++ inputs/gcp/Network Services/google_network_services_multicast_group_consumer_activation/location/compliant.tf
@@ -0,0 +1,7 @@
+resource "google_network_services_multicast_group_consumer_activation" "compliant_example_1" {
+  multicast_group_consumer_activation_id = "compliant-example-1"
+  location                               = "australia-southeast1-b"
+
+  multicast_group_range_activation = "projects/test-project/locations/australia-southeast1-b/multicastGroupRangeActivations/example-range-activation"
+  multicast_consumer_association   = "projects/test-project/locations/australia-southeast1-b/multicastConsumerAssociations/example-consumer-association"
+}
