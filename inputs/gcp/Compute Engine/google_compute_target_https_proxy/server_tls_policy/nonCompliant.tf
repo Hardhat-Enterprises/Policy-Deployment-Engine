34805df342c543db83de58d22dedcdeafd6fdb2c
gauthamrmesh <gautham.music@gmail.com>
2026-09-13 22:11:23 +1000

    Reclassify certificate_manager_certificates, certificate_map, server_tls_policy, ssl_policy as security_impact:false per tutor-confirmed reviewer feedback - not platform-enforceable without hardcoding team-specific values

--- inputs/gcp/Compute Engine/google_compute_target_https_proxy/server_tls_policy/nonCompliant.tf
+++
@@ -1,4 +0,0 @@
-resource "google_compute_target_https_proxy" "non_compliant_example_1" {
-  name    = "noncompliant-https-proxy-1"
-  url_map = "fake-url-map-1"
-}
--- inputs/gcp/Compute Engine/google_compute_target_https_proxy/ssl_policy/compliant.tf
+++
@@ -1,5 +0,0 @@
-resource "google_compute_target_https_proxy" "compliant_example_1" {
-  name        = "compliant-https-proxy-1"
-  url_map     = "fake-url-map-1"
-  ssl_policy  = "projects/project-1/global/sslPolicies/policy-1"
-}
