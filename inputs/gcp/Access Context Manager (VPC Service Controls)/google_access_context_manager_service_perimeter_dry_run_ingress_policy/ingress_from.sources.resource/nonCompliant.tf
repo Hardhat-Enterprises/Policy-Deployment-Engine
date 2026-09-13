a6fdd09c76d20fdea9a4516d0c9619b14fdcba71
sarthakghildiyal <sarthakghildiyal@gmail.com>
2026-09-13 18:34:35 +1000

    Align dry run ingress docs with review feedback

--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter_dry_run_ingress_policy/ingress_from.sources.resource/nonCompliant.tf
+++
@@ -1,9 +0,0 @@
-resource "google_access_context_manager_service_perimeter_dry_run_ingress_policy" "non_compliant_example_1" {
-  perimeter = "accessPolicies/123456/servicePerimeters/my_perimeter"
-
-  ingress_from {
-    sources {
-      resource = "folders/999999999"
-    }
-  }
-}
--- inputs/gcp/Access Context Manager (VPC Service Controls)/google_access_context_manager_service_perimeter_dry_run_ingress_policy/ingress_to.roles/nonCompliant.tf
+++
@@ -1,7 +0,0 @@
-resource "google_access_context_manager_service_perimeter_dry_run_ingress_policy" "non_compliant_example_1" {
-  perimeter = "accessPolicies/123456789/servicePerimeters/my_perimeter"
-
-  ingress_to {
-    roles = ["roles/owner"]
-  }
-}
