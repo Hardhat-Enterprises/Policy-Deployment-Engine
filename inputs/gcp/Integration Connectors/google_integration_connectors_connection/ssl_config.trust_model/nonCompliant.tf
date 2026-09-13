0d09f28ef55c0094da371dd56650da1c1dfafe97
Karim <Sulimankemo43@gmail.com>
2026-09-13 21:10:32 +1000

    Add security impact assessment for google_integration_connectors_connection

---
+++ inputs/gcp/Integration Connectors/google_integration_connectors_connection/ssl_config.trust_model/nonCompliant.tf
@@ -0,0 +1,21 @@
+resource "google_integration_connectors_connection" "non_compliant_example_1" {
+  name              = "noncompliant-conn-1"
+  location          = "australia-southeast1"
+  connector_version = "projects/test-project/locations/global/providers/zendesk/connectors/zendesk/versions/1"
+
+  auth_config {
+    auth_type = "USER_PASSWORD"
+    user_password {
+      username = "user@example.com"
+      password {
+        secret_version = "projects/test-project/secrets/conn-pw/versions/latest"
+      }
+    }
+  }
+
+  ssl_config {
+    type        = "TLS"
+    use_ssl     = true
+    trust_model = "INSECURE"
+  }
+}
