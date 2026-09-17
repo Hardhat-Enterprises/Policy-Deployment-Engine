95866d29d7cf8a8e840a58417085c9f9d4b7b092
connor-deakin <cdruiven@deakin.edu.au>
2026-09-17 15:41:23 +1000

    gcp/compute_engine/google_compute_router_nat fix: nat_ip_allocate_option removed from security policies

--- inputs/gcp/Compute Engine/google_compute_router_nat/nat_ip_allocate_option/nonCompliant.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_router_nat" "non_compliant_example_1" {
-    name = "non-compliant-example-1"
-    source_subnetwork_ip_ranges_to_nat  = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-    router = "test-router"
-    region = "australia-southeast1"
-    nat_ip_allocate_option = "AUTO_ONLY"
-}
