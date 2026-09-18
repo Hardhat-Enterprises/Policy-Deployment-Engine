3b9d7170efaca6e0057307c4460cca6d7e548512
Your Full Name <your_github_email@example.com>
2026-09-19 03:50:22 +1000

    Align Redis CA fixture and remove drift exemption

--- inputs/gcp/Memorystore (Redis)/google_redis_cluster/server_ca_mode/nonCompliant.tf
+++ inputs/gcp/Memorystore (Redis)/google_redis_cluster/server_ca_mode/nonCompliant.tf
@@ -9,4 +9,5 @@ resource "google_redis_cluster" "non_compliant_example_1" {
 
   transit_encryption_mode = "TRANSIT_ENCRYPTION_MODE_SERVER_AUTHENTICATION"
   server_ca_mode = "SERVER_CA_MODE_GOOGLE_MANAGED_PER_INSTANCE_CA"
+  server_ca_pool          = "projects/pde-project-vindya/locations/australia-southeast1/caPools/redis-ca-pool"
 }
