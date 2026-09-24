46fdb0dc24dea6e362125eeba447931336c42688
Faris Alharbi <farisalharbi.fa@gmail.com>
2026-09-24 22:21:20 +1000

    feat(cloud_deploy): add deploy policy controls

--- inputs/gcp/Cloud Deploy/google_clouddeploy_deploy_policy/deletion_policy/nonCompliant.tf
+++ inputs/gcp/Cloud Deploy/google_clouddeploy_deploy_policy/deletion_policy/nonCompliant.tf
@@ -2,7 +2,7 @@ resource "google_clouddeploy_deploy_policy" "non_compliant_example_1" {
   name            = "non_compliant_example_1"
   location        = "australia-southeast1"
   project         = "my-project-name"
-  deletion_policy = "PREVENT"
+  deletion_policy = "DELETE"
 
   selectors {
     delivery_pipeline {
@@ -13,7 +13,7 @@ resource "google_clouddeploy_deploy_policy" "non_compliant_example_1" {
     }
   }
 
-  suspended = true
+  suspended = false
 
   rules {
     rollout_restriction {
