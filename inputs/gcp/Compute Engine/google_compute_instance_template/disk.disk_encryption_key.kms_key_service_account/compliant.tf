7087a38d1bfdf48e916f45efeeb1dea8a49c269b
Rishita <rishita.yadava@gmail.com>
2026-09-24 17:13:40 +1000

    Add compute_instance_template disk.disk_encryption_key.kms_key_service_account policy

---
+++ inputs/gcp/Compute Engine/google_compute_instance_template/disk.disk_encryption_key.kms_key_service_account/compliant.tf
@@ -0,0 +1,23 @@
+resource "google_compute_instance_template" "compliant_example_1" {
+  name         = "compliant-template"
+  machine_type = "e2-medium"
+
+  disk {
+    source_image = "debian-cloud/debian-11"
+    auto_delete   = true
+    boot          = true
+    disk_encryption_key {
+      kms_key_self_link       = "projects/fake-project/locations/us-central1/keyRings/fake-ring/cryptoKeys/fake-key"
+      kms_key_service_account = "fake-sa@fake-project.iam.gserviceaccount.com"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  service_account {
+    email  = "fake-sa@fake-project.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+}
